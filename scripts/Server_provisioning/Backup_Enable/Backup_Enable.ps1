0fd9611e9bac407ee0a743d7cce2742427d7d506
Antonio Vento <antoniovicente.vento@schindler.com>
2021-03-25 11:51:29 +0100

    backup

--- scripts/Server_provisioning/Backup_Enable/Backup_Enable.ps1
+++ scripts/Server_provisioning/Backup_Enable/Backup_Enable.ps1
@@ -3,8 +3,8 @@
 #$Vault = "rsv-nonprod-euno-lrsbackup-01"
 #$policy = "vm-medium-01am-01"
 #$vm = "zzzwsr0026"
-Select-AzSubscription -Subscription $sub
-$vault = Get-AzRecoveryServicesVault -ResourceGroupName $Backup_rg -Name $Vault
-$policy = Get-AzRecoveryServicesBackupProtectionPolicy -VaultId $Vault.ID -Name $policy
-$vm = Get-AzVM -Name $vm
+Select-AzSubscription -Subscription $(sub)
+$vault = Get-AzRecoveryServicesVault -ResourceGroupName $(Backup_rg) -Name $(Vault)
+$policy = Get-AzRecoveryServicesBackupProtectionPolicy -VaultId $Vault.ID -Name $(policy)
+$vm = Get-AzVM -Name $(vm)
 Enable-AzRecoveryServicesBackupProtection -VaultId $vault.ID -Policy $policy -name $VM.Name -ResourceGroupName $vm.ResourceGroupName
