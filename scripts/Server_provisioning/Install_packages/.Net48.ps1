47aba855bceac1bbe389d8ff771a4af50de2ede0
Antonio Vento <antoniovicente.vento@schindler.com>
2021-03-29 14:18:59 +0200

    .net

--- scripts/Server_provisioning/Install_packages/.Net48.ps1
+++ scripts/Server_provisioning/Install_packages/.Net48.ps1
@@ -1,3 +1,3 @@
-cd c:\provision\Microsoft\.Net_4.8
-ndp48-x86-x64-allos-enu.exe /i /q /norestart
+cd "c:\provision\Microsoft\.Net_4.8"
+msiexec.exe /i /q /norestart ndp48-x86-x64-allos-enu.exe
 start-sleep 180
