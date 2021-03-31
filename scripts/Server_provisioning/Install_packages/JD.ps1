9c58603375a91a581218c362e177f6d2831b72a2
Antonio Vento <antoniovicente.vento@schindler.com>
2021-03-31 13:07:23 +0200

    JD

--- scripts/Server_provisioning/Install_packages/JD.ps1
+++ scripts/Server_provisioning/Install_packages/JD.ps1
@@ -1,1 +1,7 @@
-cmd.exe /c "c:\provision\schindler\jd\jdg.exe"
+$jd_User = "svcshhldmssosd"
+$PW = "76492d1116743f0423413b16050a5345MgB8AFUASQAwAFQASQBlAFkAVQBwAEEAcQA0AFQAVABMAGwAVgBpAE0ASQBRAEEAPQA9AHwAMwBlADYAMgAyAGYAMwAyAGMAZgAwADgAMQA3AGIAZgBhAGEAYwA1ADgAMgA3ADMAOQBhADgAOQAxADUAYwBiADQAMwBhADkAMwBmAGMAMgA1ADcAOQBmAGUAYQAwAGUAYgA1AGUAZQBhADQAZgAwAGIANQBjAGYAOQBmADQAOAA="
+$Key = (3, 4, 2, 3, 56, 34, 254, 222, 1, 1, 2, 23, 42, 54, 33, 233, 1, 34, 2, 7, 6, 5, 35, 43)
+$password = $PW | ConvertTo-SecureString -key $key
+$domain = "global.schindler.com"
+[pscredential]$credObject = New-Object System.Management.Automation.PSCredential($jd_User, $password)
+Add-Computer -DomainName $domain -Credential $credObject
