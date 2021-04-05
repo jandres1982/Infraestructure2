354f80373c2744317b2adc85f1bcdcffdb732a4a
Antonio Vento <antoniovicente.vento@schindler.com>
2021-04-05 15:14:44 +0200

    JD

--- scripts/Server_provisioning/Install_packages/JD.ps1
+++ scripts/Server_provisioning/Install_packages/JD.ps1
@@ -1,7 +1,18 @@
-$jd_User = "svcshhldmssosd"
-$PW = "76492d1116743f0423413b16050a5345MgB8AFUASQAwAFQASQBlAFkAVQBwAEEAcQA0AFQAVABMAGwAVgBpAE0ASQBRAEEAPQA9AHwAMwBlADYAMgAyAGYAMwAyAGMAZgAwADgAMQA3AGIAZgBhAGEAYwA1ADgAMgA3ADMAOQBhADgAOQAxADUAYwBiADQAMwBhADkAMwBmAGMAMgA1ADcAOQBmAGUAYQAwAGUAYgA1AGUAZQBhADQAZgAwAGIANQBjAGYAOQBmADQAOAA="
-$Key = (3, 4, 2, 3, 56, 34, 254, 222, 1, 1, 2, 23, 42, 54, 33, 233, 1, 34, 2, 7, 6, 5, 35, 43)
-$password = $PW | ConvertTo-SecureString -key $key
-$domain = "global.schindler.com"
-[pscredential]$credObject = New-Object System.Management.Automation.PSCredential($jd_User, $password)
-Add-Computer -DomainName $domain -Credential $credObject
+#$jd_User = "svcshhldmssosd"
+#$PW = #"76492d1116743f0423413b16050a5345MgB8AFUASQAwAFQASQBlAFkAVQBwAEEAcQA0AFQAVABMAGwAVgBpAE0ASQBRAEEAPQA9AHwAMwBlADYAMgAyAGYAMwAyAGMAZgAwADgAMQA3AGIAZgBhAGEAYwA1ADgAMgA3ADMAOQBhADgAOQAxADUAYwBiADQAMwBhADkAMwBmAGMAMgA1ADcAOQBmAGUAYQAwAGUAYgA1AGUAZQBhADQAZgAwAGIANQBjAGYAOQBmADQAOAA="
+#$Key = (3, 4, 2, 3, 56, 34, 254, 222, 1, 1, 2, 23, 42, 54, 33, 233, 1, 34, 2, 7, 6, 5, 35, 43)
+#$password = $PW | ConvertTo-SecureString -key $key
+#$domain = "global.schindler.com"
+#[pscredential]$credObject = New-Object System.Management.Automation.PSCredential($jd_User, $password)
+#Add-Computer -DomainName $domain -Credential $credObject
+$date = Get-Date
+$dt = $date.AddMinutes(1)
+$Action = New-ScheduledTaskAction -Execute 'cmd.exe' -Argument '/c "C:\provision\Schindler\JD\JDG.exe"'
+$Trigger = New-ScheduledTaskTrigger -Once -At $dt
+$Settings = New-ScheduledTaskSettingsSet
+$STPrin = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Administrators" -RunLevel Highest
+$Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings -Principal $STPrin
+Register-ScheduledTask -TaskName 'Join Domain' -InputObject $Task
+schtasks /change /tn 'Join Domain' /ru "NT AUTHORITY\SYSTEM"
+start-sleep 120
+Restart-Computer -force
