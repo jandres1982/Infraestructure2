6d2247e409d375619bd054cda934891f14a3ed9c
Antonio Vento <antoniovicente.vento@schindler.com>
2021-04-05 16:20:15 +0200

    JD1

---
+++ scripts/Server_provisioning/Install_packages/JD1.ps1
@@ -0,0 +1,12 @@
+$date = Get-Date
+$dt = $date.AddMinutes(40)
+$hostname = hostname
+$Action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument "-File C:\provision\Schindler\JD\JDG.ps1"
+$Trigger = New-ScheduledTaskTrigger -Once -At $dt
+$Settings = New-ScheduledTaskSettingsSet
+#$STPrin = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Administrators" -RunLevel Highest
+$STPrin = New-ScheduledTaskPrincipal -UserId "$hostname\ldmsosd" -RunLevel Highest
+$Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings -Principal $STPrin
+Register-ScheduledTask -TaskName 'Join Domain Test Task' -InputObject $Task
+#schtasks /change /tn 'Join Domain Test Task' /ru "NT AUTHORITY\SYSTEM"
+#schtasks /change /tn 'Join Domain Test Task' /ru "ldmsosd"
