e0231297116370313a435e467e841146173cebb1
Antonio Vento <antoniovicente.vento@schindler.com>
2021-04-21 13:33:40 +0200

    change

--- scripts/Server_provisioning/Log_Analytics_Agent/Log_Analytics_PS.ps1
+++ scripts/Server_provisioning/Log_Analytics_Agent/Log_Analytics_PS.ps1
@@ -1,13 +1,14 @@
-$PublicSettings = @{"workspaceId" = $(workspaceId)}
-$ProtectedSettings = @{"workspaceKey" = $(workspaceKey)}
+echo "$(workspaceId)"
+#$PublicSettings = @{"workspaceId" = $(workspaceId)}
+#$ProtectedSettings = @{"workspaceKey" = $(workspaceKey)}
 #fa488d5a-d8e4-4437-9ccc-2ef59e9eb669
 #1DxbXeHBAM3QLWl4GcE9SF0eTCEYuyr5pAt5k3wGG+bASH/ug9XGmVUyHKGvi/nmVIAYLLvfemwkuhM0yxGWCA==
-Set-AzVMExtension -ExtensionName "MicrosoftMonitoringAgent" `
-    -ResourceGroupName $(rg) `
-    -VMName $(vm) `
-    -Publisher "Microsoft.EnterpriseCloud.Monitoring" `
-    -ExtensionType "MicrosoftMonitoringAgent" `
-    -TypeHandlerVersion 1.0 `
-    -Settings $PublicSettings `
-    -ProtectedSettings $ProtectedSettings `
-    -Location NorthEurope
+#Set-AzVMExtension -ExtensionName "MicrosoftMonitoringAgent" `
+#    -ResourceGroupName $(rg) `
+#    -VMName $(vm) `
+#    -Publisher "Microsoft.EnterpriseCloud.Monitoring" `
+#    -ExtensionType "MicrosoftMonitoringAgent" `
+#    -TypeHandlerVersion 1.0 `
+#    -Settings $PublicSettings `
+#    -ProtectedSettings $ProtectedSettings `
+#    -Location NorthEurope
