1d56e99340d1aa0d8d6886588b3d9e688c1e3c0b
Antonio Vento <antoniovicente.vento@schindler.com>
2021-05-18 14:56:39 +0200

    MAP_Non_Prod_AM

--- scripts/Server_provisioning/Install_packages/Map_copy_AM_Non_Prod.ps1
+++ scripts/Server_provisioning/Install_packages/Map_copy_AM_Non_Prod.ps1
@@ -1,11 +1,11 @@
 
 #configure the host file with: 10.166.14.8 stnonprodgeneric0002.file.core.windows.net
-$connectTestResult = Test-NetConnection -ComputerName stnonprodgeneric0002.file.core.windows.net -Port 445
+$connectTestResult = Test-NetConnection -ComputerName 10.166.14.8 -Port 445
 if ($connectTestResult.TcpTestSucceeded) {
     # Save the password so the drive will persist on reboot
-    cmd.exe /C "cmdkey /add:`"stnonprodgeneric0002.file.core.windows.net`" /user:`"Azure\stnonprodgeneric0002`" /pass:`"GJSSDqSkGpHFMlyQFAWMoi5hRnUu7Tzzl+MfvFDiAtpgKkPzm8T7YdwnFmePwafSrJKzOhzCEZUvmj4YYyM2hw==`""
+    cmd.exe /C "cmdkey /add:`"10.166.14.8`" /user:`"Azure\stnonprodgeneric0002`" /pass:`"GJSSDqSkGpHFMlyQFAWMoi5hRnUu7Tzzl+MfvFDiAtpgKkPzm8T7YdwnFmePwafSrJKzOhzCEZUvmj4YYyM2hw==`""
     # Mount the drive
-    New-PSDrive -Name X -PSProvider FileSystem -Root "\\stnonprodgeneric0002.file.core.windows.net\servers" -Persist
+    New-PSDrive -Name X -PSProvider FileSystem -Root "\\10.166.14.8\servers" -Persist
 } else {
     Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
 }
