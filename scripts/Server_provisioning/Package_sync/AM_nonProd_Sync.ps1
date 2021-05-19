505a5963b7f2bbad80538a4084aed585a6a52a6a
Antonio Vento <antoniovicente.vento@schindler.com>
2021-05-19 16:47:02 +0200

    Sync_provision_target_persistence_changed

--- scripts/Server_provisioning/Package_sync/AM_nonProd_Sync.ps1
+++ scripts/Server_provisioning/Package_sync/AM_nonProd_Sync.ps1
@@ -1,12 +1,12 @@
 ﻿$source = "d:\provision"
-$target = "q:\provision"
+$target = "\\10.166.14.8\servers"
 $log = "c:\temp\provision_sync_AM_nonProd.txt"
 $connectTestResult = Test-NetConnection -ComputerName 10.166.14.8 -Port 445
 if ($connectTestResult.TcpTestSucceeded) {
     # Save the password so the drive will persist on reboot
     cmd.exe /C "cmdkey /add:`"10.166.14.8`" /user:`"Azure\stnonprodgeneric0002`" /pass:`"GJSSDqSkGpHFMlyQFAWMoi5hRnUu7Tzzl+MfvFDiAtpgKkPzm8T7YdwnFmePwafSrJKzOhzCEZUvmj4YYyM2hw==`""
     # Mount the drive
-    New-PSDrive -Name Q -PSProvider FileSystem -Root "\\10.166.14.8\servers" -Persist
+    New-PSDrive -Name Q -PSProvider FileSystem -Root "\\10.166.14.8\servers"
 } else {
     Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
 }
--- scripts/Server_provisioning/Package_sync/EU_nonProd_Sync.ps1
+++ scripts/Server_provisioning/Package_sync/EU_nonProd_Sync.ps1
@@ -1,12 +1,12 @@
 ﻿$source = "d:\provision"
-$target = "m:\provision"
+$target = "\\10.37.14.28\servers"
 $log = "c:\temp\provision_sync_EU_nonProd.txt"
 $connectTestResult = Test-NetConnection -ComputerName 10.37.14.28 -Port 445
 if ($connectTestResult.TcpTestSucceeded) {
     # Save the password so the drive will persist on reboot
     cmd.exe /C "cmdkey /add:`"10.37.14.28`" /user:`"Azure\stnonprodgeneric0001`" /pass:`"553DvxEiHCz07QEFvgmiUHxoHdXF1htL5Lu/KqWlHkBib9exZ8ClmXVwaz+nOFBhuNy0Uskes/miVH0lIw9bqA==`""
     # Mount the drive
-    New-PSDrive -Name M -PSProvider FileSystem -Root "\\10.37.14.28\servers" -Persist
+    New-PSDrive -Name M -PSProvider FileSystem -Root "\\10.37.14.28\servers"
 } else {
     Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
 }
