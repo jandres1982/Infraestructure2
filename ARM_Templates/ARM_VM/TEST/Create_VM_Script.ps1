07f6d719a550f3f0fae4b4511ba468a7631763cd
Antonio Vento <antoniovicente.vento@schindler.com>
2021-05-24 22:58:38 +0200

    check

--- ARM_Templates/ARM_VM/TEST/Create_VM_Script.ps1
+++ ARM_Templates/ARM_VM/TEST/Create_VM_Script.ps1
@@ -17,9 +17,9 @@ $Parameters = Get-Content $Parameters_Base | out-string | ConvertFrom-Json
 $Parameters.parameters.virtualMachineName.value = "$vm"
 $Parameters.parameters.networkInterfaceName.value = "$vm`_01"
 
-$Parameters | ConvertTo-Json | Out-File -FilePath Parameters_$vm`.json -Encoding utf8 -Force
+$Parameters | ConvertTo-Json | Out-File -FilePath ".\Parameters.json" -Encoding utf8 -Force
 
-New-AzResourceGroupDeployment -ResourceGroupName $rg -TemplateFile $Template_2019 -TemplateParameterFile Parameters_$vm`.json
+New-AzResourceGroupDeployment -ResourceGroupName $rg -TemplateFile $Template_2019 -TemplateParameterFile ".\parameters.json"
 
 #command to create a VM
 #}
