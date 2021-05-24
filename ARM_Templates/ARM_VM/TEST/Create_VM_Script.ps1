155c66e56d9bc6c1e0d120a48aa3e5982225b2e3
Antonio Vento <antoniovicente.vento@schindler.com>
2021-05-24 22:27:26 +0200

    check

--- ARM_Templates/ARM_VM/TEST/Create_VM_Script.ps1
+++ ARM_Templates/ARM_VM/TEST/Create_VM_Script.ps1
@@ -3,19 +3,19 @@ $vm = $args[0]
 $rg = $args[1]
 #$server_list = gc $(System.DefaultWorkingDirectory)/_Infraestructure/ARM_Templates/ARM_VM/TEST/server_list.txt
 $Parameters_Base = "parameters.json"
-
 $Template_2019 = "template_2019.json"
 
+Write-Output $vm
 ##########################################################################
-$Parameters = Get-Content $Parameters_Base | out-string | ConvertFrom-Json
+#$Parameters = Get-Content $Parameters_Base | out-string | ConvertFrom-Json
 
 #foreach ($server in $server_list)
 #{
 
-$Parameters.parameters.virtualMachineName.value = "$vm"
-$Parameters.parameters.networkInterfaceName.value = "$vm`_01"
+#$Parameters.parameters.virtualMachineName.value = "$vm"
+#$Parameters.parameters.networkInterfaceName.value = "$vm`_01"
 
-New-AzResourceGroupDeployment -ResourceGroupName $rg -TemplateFile $Template_2019 -TemplateParameterFile $Parameters
+#New-AzResourceGroupDeployment -ResourceGroupName $rg -TemplateFile $Template_2019 -TemplateParameterFile $Parameters
 
 #command to create a VM
 #}
