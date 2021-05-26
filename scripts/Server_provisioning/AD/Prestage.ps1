29d9004032c4a10b836eccf1c5ac9f965e3d992d
Antonio Vento <antoniovicente.vento@schindler.com>
2021-05-26 16:40:08 +0200

    prestage

---
+++ scripts/Server_provisioning/AD/Prestage.ps1
@@ -0,0 +1,3 @@
+$vm = $args[0]
+$Function = $args[1]
+New-ADComputer -Name $vm -Path "OU=EU,OU=Servers,OU=NBI12,DC=global,DC=schindler,DC=com" -PasswordNotRequired $false -Description $Function -ErrorAction SilentlyContinue
