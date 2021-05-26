99a5dafd724edec10c11270be08df965c3137c40
Antonio Vento <antoniovicente.vento@schindler.com>
2021-05-26 17:04:31 +0200

    prestage

--- scripts/Server_provisioning/AD/Prestage.ps1
+++ scripts/Server_provisioning/AD/Prestage.ps1
@@ -1,5 +1,5 @@
 $vm = $args[0]
 $Function = $args[1]
-#New-ADComputer -Name $vm -Path "OU=EU,OU=Servers,OU=NBI12,DC=global,DC=schindler,DC=com" -PasswordNotRequired $false -Description $Function -ErrorAction SilentlyContinue
+New-ADComputer -Name $vm -Path "OU=EU,OU=Servers,OU=NBI12,DC=global,DC=schindler,DC=com" -PasswordNotRequired $false -Description $Function -ErrorAction SilentlyContinue
 
 write-host "$vm and $Function"
