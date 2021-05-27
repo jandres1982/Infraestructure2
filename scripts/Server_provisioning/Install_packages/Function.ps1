b6a06b7951b87ca4fe0254d91e543fc943776841
Antonio Vento <antoniovicente.vento@schindler.com>
2021-05-27 11:42:37 +0200

    Function

--- scripts/Server_provisioning/Install_packages/Function.ps1
+++ scripts/Server_provisioning/Install_packages/Function.ps1
@@ -1,1 +1,8 @@
-az vm run-command invoke  --command-id RunPowerShellScript --name $(vm) -g $(rg) --scripts "$hostname = hostname; $hostname = $hostname.Toupper(); $KG = $hostname.Substring(0,3); $Description = "$KG Windows Server $(function)"; $OSWMI=Get-WmiObject -class Win32_OperatingSystem; $OSWMI.Description = $Description; $OSWMI.put()"
+$function = $arg(0)
+$hostname = hostname
+$hostname = $hostname.Toupper()
+$KG = $hostname.Substring(0,3)
+$Description = "$KG Windows Server $function"
+$OSWMI=Get-WmiObject -class Win32_OperatingSystem
+$OSWMI.Description = $Description
+$OSWMI.put()
