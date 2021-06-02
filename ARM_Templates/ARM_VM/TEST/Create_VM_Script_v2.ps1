c1b86fe0a2afc2cd0a14bf9b0a1f43ad2c5e90b3
Antonio Vento <antoniovicente.vento@schindler.com>
2021-06-02 13:11:59 +0200

    New_PS_File

---
+++ ARM_Templates/ARM_VM/TEST/Create_VM_Script_v2.ps1
@@ -0,0 +1,34 @@
+# Write your PowerShell commands here.
+$vm = $args[0]
+$rg = $args[1]
+#$server_list = gc $(System.DefaultWorkingDirectory)/_Infraestructure/ARM_Templates/ARM_VM/TEST/server_list.txt
+$Parameters_Base = ".\parameters.json"
+$Template = ".\template_2019.json"
+
+#Write-Output $vm
+
+#Write-host $Parameters_Base
+##########################################################################
+$json = Get-Content $Parameters_Base -raw | convertfrom-json
+$json.parameters
+#$json = $Parameters_Base | convertfrom-json
+#$json.parameters.virtualMachineName.value = $Vm
+#$json.parameters.virtualMachineComputerName.value = $Vm
+#$json.parameters.virtualMachineRG.value = $rg
+#$json.parameters.networkInterfaceName.value = "$vm`_01"
+#$json.parameters
+
+
+
+#foreach ($server in $server_list)
+#{
+
+#$Parameters.parameters.virtualMachineName.value = "$vm"
+#$Parameters.parameters.networkInterfaceName.value = "$vm`_01"
+
+#$Parameters | ConvertTo-Json | Out-File -FilePath ".\Parameters.json" -Encoding utf8 -Force
+
+#New-AzResourceGroupDeployment -ResourceGroupName $rg -TemplateFile $Template_2019 -TemplateParameterFile ".\parameters.json"
+
+#command to create a VM
+#}
