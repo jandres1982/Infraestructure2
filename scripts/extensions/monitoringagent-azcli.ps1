80fa2c784a09d56d6f18bdc0efb07152507fa27a
Nahum Sancho Puri <nahum.sancho@schindler.com>
2021-06-07 18:22:02 +0000

    Updated monitoringagent-azcli.ps1

--- scripts/extensions/monitoringagent-azcli.ps1
+++ scripts/extensions/monitoringagent-azcli.ps1
@@ -1,9 +1,5 @@
 ##Subscription##
-$subscription = "s-sis-eu-prod-01"
 az account set --subscription $subscription
-
-$rg = "rg-cis-prod-server-01"
-$vm = "SHHWSR1999"
 
 #install extension##
 az vm extension set -n MicrosoftMonitoringAgent --publisher Microsoft.EnterpriseCloud.Monitoring --version 1.0.18053.0 --vm-name $vm --resource-group $rg 
