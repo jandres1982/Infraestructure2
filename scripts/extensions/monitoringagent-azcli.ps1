8508c95d626d7a43beaddc75e58956384a83793d
Nahum Sancho Puri <nahum.sancho@schindler.com>
2021-06-07 18:28:24 +0000

    Updated monitoringagent-azcli.ps1 with correct tag values

--- scripts/extensions/monitoringagent-azcli.ps1
+++ scripts/extensions/monitoringagent-azcli.ps1
@@ -1,5 +1,5 @@
 ##Subscription##
-az account set --subscription $subscription
+az account set --subscription $(subscription)
 
 #install extension##
-az vm extension set -n MicrosoftMonitoringAgent --publisher Microsoft.EnterpriseCloud.Monitoring --version 1.0.18053.0 --vm-name $vm --resource-group $rg 
+az vm extension set -n MicrosoftMonitoringAgent --publisher Microsoft.EnterpriseCloud.Monitoring --version 1.0.18053.0 --vm-name $(vm) --resource-group $(rg) 
