55186cde1006e573a5c01e7418695949cf463a52
Antonio Vento <antoniovicente.vento@schindler.com>
2021-06-11 11:26:55 +0200

    DMZ_Config

--- scripts/Server_provisioning/Install_packages/Qualys_config_dmz.ps1
+++ scripts/Server_provisioning/Install_packages/Qualys_config_dmz.ps1
@@ -1,4 +1,9 @@
-netsh winhttp import proxy source=ie
+#netsh winhttp import proxy source=ie
+#cd "C:\Program Files\Qualys\QualysAgent"
+#.\QualysProxy.exe /h
+#Restart-Service -Name QualysAgent -Force
+
+$Get_proxy = [System.Net.WebProxy]::GetDefaultProxy()
+$Proxy = $Get_proxy.Address.Authority
 cd "C:\Program Files\Qualys\QualysAgent"
-.\QualysProxy.exe /h
-Restart-Service -Name QualysAgent -Force
+.\QualysProxy.exe /u $Proxy
