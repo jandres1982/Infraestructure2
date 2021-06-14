039dc89e2e75f06f6abb312a851723efa948409b
Antonio Vento <antoniovicente.vento@schindler.com>
2021-06-14 12:33:59 +0200

    MM EU Prod

---
+++ scripts/MMA/MMA_ADD_SCC.ps1
@@ -0,0 +1,9 @@
+function addWorkID_SCC
+{
+    $workspaceId = "a054b1bf-24eb-4e0b-a7e6-0fb782e77bf6"
+    $workspaceKey = "d/DAXhGLEcj+D+QNvTQj84jWCEcAb+jsn/37lroX0zjfBEQBIOppTeIglpqCeCHz6a/XXcyY8QgUvmrOkV+dmg=="
+    $mma1 = New-Object -ComObject 'AgentConfigManager.MgmtSvcCfg'
+    $mma1.AddCloudWorkspace($workspaceId, $workspaceKey)
+    $mma1.ReloadConfiguration()
+}
+addWorkID_SCC
