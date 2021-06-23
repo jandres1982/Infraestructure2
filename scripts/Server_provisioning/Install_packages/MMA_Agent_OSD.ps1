c2ee3bc717b70a1b74d3ceb7c92680cafd38d46c
Antonio Vento <antoniovicente.vento@schindler.com>
2021-06-23 15:59:48 +0200

    MMA_OSD_agent

--- scripts/Server_provisioning/Install_packages/MMA_Agent_OSD.ps1
+++ scripts/Server_provisioning/Install_packages/MMA_Agent_OSD.ps1
@@ -1,3 +1,4 @@
+New-Item -Path ".\MMA_Json" -ItemType "Directory" -Force
 $vm = $args[0]
 $Parameters_Base = ".\parameters-eu.json"
 $Template = ".\template.json"
@@ -6,3 +7,5 @@ $json.parameters.vmName[0].value = "$vm"
 $json | ConvertTo-Json -Depth 32 | Out-File -encoding "UTF8" -FilePath ".\MMA_Json\$vm.json"
 New-AzResourceGroupDeployment -ResourceGroupName $rg -TemplateParameterFile ".\MMA_Json\$vm.json" -TemplateFile $Template
 Write-Host "No agent was found in $vm, but now should be enable"
+
+#In PowerShell As an Administrator go to this path “C:\Program Files\Microsoft Monitoring Agent\Agent\Troubleshooter” and run the script “GetAgentInfo.PS1” and choose option “Collect all logs”
