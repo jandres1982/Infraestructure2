55304e73e29673d556251a75315107a153e8f5dd
Antonio Vicente Vento Maggio <antoniovicente.vento@schindler.com>
2021-06-23 21:34:26 +0000

    Updated psemail.ps1

--- scripts/Server_provisioning/psemail/psemail.ps1
+++ scripts/Server_provisioning/psemail/psemail.ps1
@@ -8,8 +8,13 @@ $Subject = "Server $vm is completed from Devops Script at $Date "
 #$Filename = Get-ChildItem $Path -Name "Att*" | select -Last 1
 #$Attachment = "$Path$Filename"
 $Body = @"
-"@
+This mail is being generated automatically by Schindler Powershell Script SPS (ventoa1)
 
+Please check all the software has been Installed, in case you find any problems, please contact the Server Team.
+
+SCC Server Competence Center - Schindler Support
+
+"@
 #Send-MailMessage -From $From -To $To -Subject $Subject -Body $Body -Attachments $Attachment
 
 Send-MailMessage -From $From -To $To -Subject $Subject -Body $Body
