bc60fc786a32ec61ff30054233723cbb26693553
Antonio Vento <antoniovicente.vento@schindler.com>
2021-06-24 09:39:09 +0200

    PSemail

--- scripts/Server_provisioning/psemail/psemail.ps1
+++ scripts/Server_provisioning/psemail/psemail.ps1
@@ -2,16 +2,18 @@ $vm = $args[0]
 $rg = $args[1]
 $re_email = $args[2]
 $PSEmailServer = "smtp.eu.schindler.com"
-$From = "david.sanchoiguaz@schindler.com"
-
+$From = "scc-support-zar.es@schindler.com"
 $To = "0034691022611@sms.schindler.com","$re_email"
 $Subject = "Server $vm was completed with Schindler Devops Script on $rg"
 #$Path = "D:\Repository\Working\Antonio\PS_Email\Test_attachments\"
 #$Filename = Get-ChildItem $Path -Name "Att*" | select -Last 1
 #$Attachment = "$Path$Filename"
 $Body = @"
-Server $vm has been provisioned in $rg, please check backup is enable and the correct IP address.
-Remeber to create the SIM local admin group and added to the server
+Server $vm has been provisioned in $rg. 
+- Check Backup is Enabled.
+- Check SIM local admin group is added.
+- Check Updates were Installed.
+- Check MMA agent is connected to workspaces.
 "@
 #Send-MailMessage -From $From -To $To -Subject $Subject -Body $Body -Attachments $Attachment
 
