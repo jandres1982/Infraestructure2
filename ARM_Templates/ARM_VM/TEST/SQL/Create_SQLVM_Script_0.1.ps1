7b2590c1212f094e75dfaafea536f625e2ae7969
Alberto <alberto.delgado@schindler.com>
2021-07-01 12:23:59 +0200

    Sqlv01

--- ARM_Templates/ARM_VM/TEST/SQL/Create_SQLVM_Script_0.1.ps1
+++ ARM_Templates/ARM_VM/TEST/SQL/Create_SQLVM_Script_0.1.ps1
@@ -4,16 +4,15 @@ $vm = $args[0]
 $rg = $args[1]
 $vm_size = $args[2]
 $subnet = $args[3]
-$disk_size = $args[4]
+$sysdb_0 = $args[4]
+$userdb_1 = $args[5]
+$logdb_2 = $args[6]
+$tempdb_3 = $args[7]
 
-
-$Parameters_Base = ".\sql\parameters.json"
-$Template_2019 = ".\sql\template.json"
-
+$Parameters_Base = ".\parameters.json"
+$Template_2019 = ".\template.json"
 =======
-$Template = ".\template_2019.json"
-#Write-Output $vm
-#Write-host $Parameters_Base
+$Template = ".\template_2021.json"
 New-Item -ItemType directory -Path ".\server_json" -ErrorAction SilentlyContinue
 ##########################################################################
 $Parameters = Get-Content $Parameters_Base | out-string | ConvertFrom-Json
@@ -24,9 +23,23 @@ $json.parameters.subnetName.value = $subnet
 $json.parameters.virtualMachineName.value = $vm
 $json.parameters.virtualMachineComputerName.value = $vm
 $json.parameters.virtualMachineRG.value = $rg
-$json.parameters.dataDisks.value[0].name = "$vm`_DataDisk_0"
-$json.parameters.dataDiskResources.value[0].name = "$vm`_DataDisk_0"
-$json.parameters.dataDiskResources.value[0].properties[0].diskSizeGB = "$disk_size"
+
+$json.parameters.dataDisks.value[0].name = "$vm`_sysdb_0"
+$json.parameters.dataDiskResources.value[0].name = "$vm`_sysdb_0"
+$json.parameters.dataDiskResources.value[0].properties[0].diskSizeGB = "$sysdb_0"
+
+$json.parameters.dataDisks.value[1].name = "$vm`_userdb_1"
+$json.parameters.dataDiskResources.value[1].name = "$vm`_userdb_1"
+$json.parameters.dataDiskResources.value[1].properties[0].diskSizeGB = "$userdb_1"
+
+$json.parameters.dataDisks.value[2].name = "$vm`_logdb_2"
+$json.parameters.dataDiskResources.value[2].name = "$vm`_logdb_2"
+$json.parameters.dataDiskResources.value[2].properties[0].diskSizeGB = "$logdb_2"
+
+$json.parameters.dataDisks.value[3].name = "$vm`_tempdb_3"
+$json.parameters.dataDiskResources.value[3].name = "$vm`_tempdb_3"
+$json.parameters.dataDiskResources.value[3].properties[0].diskSizeGB = "$tempdb_3"
+
 $json.parameters.virtualMachineSize.value = $vm_size
 
 $json | ConvertTo-Json -Depth 32 | Out-File -encoding "UTF8" -FilePath ".\server_json\$vm.json"
@@ -39,12 +52,5 @@ $Parameters.parameters.virtualMachineName.value = "$vm"
 $Parameters.parameters.networkInterfaceName.value = "$vm`_01"
 
 $Parameters | ConvertTo-Json | Out-File -FilePath ".\Parameters.json" -Encoding utf8 -Force
-#command to create a V
+#command to create a Vm
 New-AzResourceGroupDeployment -ResourceGroupName $rg -TemplateFile $Template_2019 -TemplateParameterFile ".\parameters.json"
-
-
-#$server_list = gc $(System.DefaultWorkingDirectory)/_Infraestructure/ARM_Templates/ARM_VM/TEST/server_list.txt
-#foreach ($server in $server_list)
-#{
-#command to create a VM
-#}
