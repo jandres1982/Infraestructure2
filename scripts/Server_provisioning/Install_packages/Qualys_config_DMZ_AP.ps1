e89660ccacadc2343b08add5d0ea2ccf76f802fb
Antonio Vento <antoniovicente.vento@schindler.com>
2021-08-04 11:24:52 +0200

    Qualys_DMZ_Proxy

---
+++ scripts/Server_provisioning/Install_packages/Qualys_config_DMZ_AP.ps1
@@ -0,0 +1,17 @@
+#netsh winhttp import proxy source=ie
+#cd "C:\Program Files\Qualys\QualysAgent"
+#.\QualysProxy.exe /h
+#Restart-Service -Name QualysAgent -Force
+
+$Proxy = "http://slsnwg1000.global.schindler.com:3128"
+$registryPath = "HKLM:\Software\qualys\Proxy"
+$Name = "URL"
+$value = $Proxy
+New-Item -Path $registryPath -Force
+New-ItemProperty -Path $registryPath -Name $name -Value $value -Force
+Restart-Service -Name QualysAgent -Force
+
+#netsh winhttp reset proxy
+
+#netsh winhttp show proxy
+
