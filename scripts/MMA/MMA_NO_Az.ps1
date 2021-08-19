83197478818d30820d280962fc2d0bb670f06bf3
Antonio Vento <antoniovicente.vento@schindler.com>
2021-08-19 16:02:13 +0200

    MMA_No_AZ

--- scripts/MMA/MMA_NO_Az.ps1
+++ scripts/MMA/MMA_NO_Az.ps1
@@ -1,25 +1,27 @@
-# Migration MMA Script
-
-###
-### Functions
-###
-
-# Check MMA Version
-
-function dependency_agent
+function install_dependency_agent
 {
-cmd.exe /c 'c:\temp\azure\InstallDependencyAgent-Windows.exe /S /RebootMode=Manual'
+    $server = hostname
+If (Test-Path -Path "C:\temp\azure\InstallDependencyAgent-Windows.exe" -ErrorAction SilentlyContinue)
+            {
+            cmd.exe /c 'C:\temp\Azure\InstallDependencyAgent-windows.exe /S /RebootMode=Manual'
+            If (Get-Service -Name MicrosoftDependencyAgent -ErrorAction SilentlyContinue)
+              {
+                Write-host "$Server, Dependency Agent Installed" -ForegroundColor Green
+            }
+            else
+            {
+                Write-host "$Server, Dependency Agent Installed" -ForegroundColor Yellow
+            }
+}else
+{
+    Write-host "$Server, Dependency Agent File not found" -ForegroundColor Yellow
 }
-
+}
 
 function add_proxy
 {
 param($ProxyDomainName="webgateway-eu.schindler.com:3128")
-
-# First we get the Health Service configuration object.  We need to determine if we
-# have the right update rollup with the API we need.  If not, no need to run the rest of the script.
 $healthServiceSettings = New-Object -ComObject 'AgentConfigManager.MgmtSvcCfg'
-
 $proxyMethod = $healthServiceSettings | Get-Member -Name 'SetProxyInfo'
 
 if (!$proxyMethod)
@@ -27,50 +29,43 @@ if (!$proxyMethod)
     Write-Output 'Health Service proxy API not present, will not update settings.'
     return
 }
-
 Write-Output "Clearing proxy settings."
 $healthServiceSettings.SetProxyInfo('', '', '')
-
 Write-Output "Setting proxy to $ProxyDomainName"
 $healthServiceSettings.SetProxyInfo($ProxyDomainName, "","")
-
 }
-
-
-
 function CheckMMAver
 {
-    param([string]$actualver)
-    $ver = "NO Installed"
-    $mmaversion = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Microsoft Operations Manager\3.0\Setup" -ErrorAction SilentlyContinue
-    $agentver = $mmaversion.AgentVersion
-    if ($agentver -is [string])
-    {
-        $verarray= $agentver.Split(".")
-        $actualverarray=$actualver.Split(".")
-        for($index = 0; $index -lt $verarray.count; $index++)
-        {
-            $version = [int]$verarray[$index]
-            $actualversion = [int]$actualverarray[$index]
-            if ($version -gt $actualversion)
-            {
-                $ver = "Greater"
-                break
-            }
-            if ($version -lt $actualversion)
-            {
-                $ver = "Smaller"
-                break
-            }
-            if ($version -eq $actualversion)
-            {
-                $ver = "Same"
-            }
-        }
+ param([string]$actualver)
+ $ver = "NO Installed"
+ $mmaversion = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Microsoft Operations Manager\3.0\Setup" -ErrorAction SilentlyContinue
+ $agentver = $mmaversion.AgentVersion
+ if ($agentver -is [string])
+ {
+     $verarray= $agentver.Split(".")
+     $actualverarray=$actualver.Split(".")
+     for($index = 0; $index -lt $verarray.count; $index++)
+     {
+         $version = [int]$verarray[$index]
+         $actualversion = [int]$actualverarray[$index]
+         if ($version -gt $actualversion)
+         {
+             $ver = "Greater"
+             break
+         }
+         if ($version -lt $actualversion)
+         {
+             $ver = "Smaller"
+             break
+         }
+         if ($version -eq $actualversion)
+         {
+             $ver = "Same"
+         }
      }
-     return ($ver)
+  }
+  return ($ver)
 }
-
 function addWorkID_Migration
 {
     $workspaceId = "a054b1bf-24eb-4e0b-a7e6-0fb782e77bf6"
@@ -79,15 +74,13 @@ function addWorkID_Migration
     $mma1.AddCloudWorkspace($workspaceId, $workspaceKey)
     $mma1.ReloadConfiguration()
 }
-
 # Function Install new MMA - WorkID MIG
 function InstallNewMMA_MIG
 {
     New-Item -ItemType Directory -Force "c:\TEMP\Azure\MMA"
-    cmd.exe /c '"MMASetup-AMD64.exe" /c /t:c:\TEMP\Azure\MMA'
+    cmd.exe /c '"C:\temp\Azure\MMASetup-AMD64.exe" /c /t:c:\TEMP\Azure\MMA'
     cmd.exe /c 'C:\TEMP\Azure\MMA\Setup.exe /qn NOAPM=1 ADD_OPINSIGHTS_WORKSPACE=1 OPINSIGHTS_WORKSPACE_AZURE_CLOUD_TYPE=0 OPINSIGHTS_PROXY_URL="http://webgateway-eu.schindler.com:3128" OPINSIGHTS_WORKSPACE_ID="a054b1bf-24eb-4e0b-a7e6-0fb782e77bf6" OPINSIGHTS_WORKSPACE_KEY="d/DAXhGLEcj+D+QNvTQj84jWCEcAb+jsn/37lroX0zjfBEQBIOppTeIglpqCeCHz6a/XXcyY8QgUvmrOkV+dmg==" AcceptEndUserLicenseAgreement=1"'
 }
-
 #Function Secure .Net
 function Secure_dNET
 {
@@ -102,34 +95,27 @@ function Secure_dNET
         New-ItemProperty -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\.NETFramework\v4.0.30319' -Name SchUseStrongCrypto -PropertyType DWORD -Value 1 -ErrorAction SilentlyContinue
     }
 }
-
-# Function Update MMA
 function UpdateMMA
 {
     New-Item -ItemType Directory -Force "c:\TEMP\Azure\MMA"
-    cmd.exe /c '"MMASetup-AMD64.exe" /c /t:c:\TEMP\Azure\MMA'
+    cmd.exe /c '"C:\temp\Azure\MMASetup-AMD64.exe" /c /t:c:\TEMP\Azure\MMA'
     cmd.exe /c 'C:\TEMP\Azure\MMA\Setup.exe /qn /l*v c:\temp\azure\AgentUpgrade.log AcceptEndUserLicenseAgreement=1'
 }          
-
-
-###
 ### Main
-###
-
 $actualver = "10.20.18053.0"
 $version = CheckMMAver($actualver)
-Remove-Item -Path "c:\TEMP\Azure\MMA" -Recurse -Force
+Remove-Item -Path "c:\TEMP\Azure\MMA" -Recurse -Force -ErrorAction SilentlyContinue
 switch ($version)
 {
     "NO Installed"
     {
         InstallNewMMA_MIG
-        #dependency_agent
+	install_dependency_agent
     }
     "Greater"
     {
         addWorkID_Migration
-        #dependency_agent
+	install_dependency_agent
     }
     "Smaller"
     {
@@ -138,14 +124,13 @@ switch ($version)
         sleep 10
         addWorkID_Migration
         add_proxy
-        #dependency_agent
+	install_dependency_agent
         
     }
     "Same"
     {
         addWorkID_Migration
         add_proxy
-        #dependency_agent
+	install_dependency_agent
     }
 }
- 
