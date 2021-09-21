cb48ba2931d92f7adaf5e50f84419ca536fdb48d
Antonio Vento <antoniovicente.vento@schindler.com>
2021-09-21 11:41:41 +0200

    Prestage DMZ check with cred

--- scripts/Server_provisioning/AD/Prestage_DMZ.ps1
+++ scripts/Server_provisioning/AD/Prestage_DMZ.ps1
@@ -1,14 +1,14 @@
 param([string]$vm,[string]$function)
-#$User = "svcshhldmssosd"
-#$PW = "76492d1116743f0423413b16050a5345MgB8AHoATgBxAEgAYgBHAFkAbQAvAEQARgBrACsAMABvAGQAUgBPAGgAOABtAHcAPQA9AHwAZAA0AGEANAAxAGIAOQBhADAAOQA5ADIAZQA5ADMAZgAyADUAYgA0ADgANwAxAGUANwA0ADcANAA1AGEAZABkADkAZgA1ADAANgAwAGUAMgBjAGQAZgBiADkAMwAzAGIAMgAzADYAOQAzADkANQA4ADIAYwA1AGQAOAA2ADAAYgA="
-#$Key = (5,4,2,4,56,34,254,222,6,5,2,23,42,54,33,233,1,35,2,7,6,4,35,43)
-#$password = $PW | ConvertTo-SecureString -key $key
-#$domain = "dmz2.schindler.com"
-#$cred = New-Object System.Management.Automation.PSCredential -ArgumentList ($User, $password)
+$User = "svcshhldmssosd"
+$PW = "76492d1116743f0423413b16050a5345MgB8AHoATgBxAEgAYgBHAFkAbQAvAEQARgBrACsAMABvAGQAUgBPAGgAOABtAHcAPQA9AHwAZAA0AGEANAAxAGIAOQBhADAAOQA5ADIAZQA5ADMAZgAyADUAYgA0ADgANwAxAGUANwA0ADcANAA1AGEAZABkADkAZgA1ADAANgAwAGUAMgBjAGQAZgBiADkAMwAzAGIAMgAzADYAOQAzADkANQA4ADIAYwA1AGQAOAA2ADAAYgA="
+$Key = (5,4,2,4,56,34,254,222,6,5,2,23,42,54,33,233,1,35,2,7,6,4,35,43)
+$password = $PW | ConvertTo-SecureString -key $key
+$domain = "dmz2.schindler.com"
+$cred = New-Object System.Management.Automation.PSCredential -ArgumentList ($User, $password)
 $vm = $vm.ToUpper()
 $KG = $vm.Substring(0,3)
 $function = "$KG Windows Server $function"
-New-ADComputer -Name $vm -Path "OU=000,OU=Servers,OU=NBI12,DC=dmz2,DC=schindler,DC=com" -PasswordNotRequired $false -Description $function
+New-ADComputer -Name $vm -Path "OU=000,OU=Servers,OU=NBI12,DC=dmz2,DC=schindler,DC=com" -PasswordNotRequired $false -Description $function -credential $cred
 write-host "$vm and $Function"
 
 ##script to prestage and group
@@ -16,4 +16,4 @@ write-host "$vm and $Function"
 $Admin_Head = $KG+"_RES_SY_"
 $Admin_Tail="_ADMIN"
 $Admin_Group = "$Admin_Head$vm$Admin_Tail"
-New-ADGroup -Name $Admin_Group -GroupCategory Security -GroupScope Universal -DisplayName "$vm Administrators" -Path "OU=RES,OU=Groups,OU=Admin_Global,OU=NBI12,DC=dmz2,DC=schindler,DC=com" -Description "$vm Administrators"
+New-ADGroup -Name $Admin_Group -GroupCategory Security -GroupScope Universal -DisplayName "$vm Administrators" -Path "OU=RES,OU=Groups,OU=Admin_Global,OU=NBI12,DC=dmz2,DC=schindler,DC=com" -Description "$vm Administrators" -credential $cred
