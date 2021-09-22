942d4da354e40e8b518c87abe83b7c9fbfaa50a0
Antonio Vento <antoniovicente.vento@schindler.com>
2021-09-22 17:13:25 +0200

    Set_PF_T_Fix

--- scripts/Server_provisioning/Install_packages/Set_PF_T.ps1
+++ scripts/Server_provisioning/Install_packages/Set_PF_T.ps1
@@ -1,3 +1,5 @@
-Get-Partition -DriveLetter D | Set-Partition -NewDriveLetter T
+$Temp_Disk = Get-volume | Where-Object -Property FileSystemLabel  -eq -Value "Temporary Storage"
+$Letter = $Temp_Disk.DriveLetter
+Get-Partition -DriveLetter $Letter | Set-Partition -NewDriveLetter T
 Set-Itemproperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management' -Name 'PagingFiles' -value 'T:\pagefile.sys 0 0'
 restart-computer -force
