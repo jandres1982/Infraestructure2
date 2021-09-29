d166a483da35a95a73e61cdc3ca50f79404f2c3e
Antonio Vicente Vento Maggio <antoniovicente.vento@schindler.com>
2021-09-29 13:07:12 +0000

    Updated SQL_Disks.ps1

--- scripts/Server_provisioning/SQL/SQL_Disks.ps1
+++ scripts/Server_provisioning/SQL/SQL_Disks.ps1
@@ -4,16 +4,16 @@ Initialize-Disk -Number "4" -PartitionStyle GPT
 Initialize-Disk -Number "5" -PartitionStyle GPT
 
 New-Partition -AssignDriveLetter "2" -UseMaximumSize |
-Format-Volume -FileSystem NTFS -AllocationUnitSize 65536 -NewFileSystemLabel “SYSDB” -Confirm:$false
+Format-Volume -FileSystem NTFS -AllocationUnitSize 65536 -NewFileSystemLabel SYSDB -Confirm:$false
 
 New-Partition -AssignDriveLetter "3" -UseMaximumSize |
-Format-Volume -FileSystem NTFS -AllocationUnitSize 65536 -NewFileSystemLabel “USERDB” -Confirm:$false
+Format-Volume -FileSystem NTFS -AllocationUnitSize 65536 -NewFileSystemLabel USERDB -Confirm:$false
 
 New-Partition -AssignDriveLetter "4" -UseMaximumSize |
-Format-Volume -FileSystem NTFS -AllocationUnitSize 65536 -NewFileSystemLabel “LOGDB” -Confirm:$false
+Format-Volume -FileSystem NTFS -AllocationUnitSize 65536 -NewFileSystemLabel LOGDB -Confirm:$false
 
 New-Partition -AssignDriveLetter "5" -UseMaximumSize |
-Format-Volume -FileSystem NTFS -AllocationUnitSize 65536 -NewFileSystemLabel “TEMPDB” -Confirm:$false
+Format-Volume -FileSystem NTFS -AllocationUnitSize 65536 -NewFileSystemLabel TEMPDB -Confirm:$false
 
 # Initialize-Disk -PartitionStyle GPT -PassThru
 
