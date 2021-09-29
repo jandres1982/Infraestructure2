6dcf795aedf10f19d78a6dff7ef5a19b952e8c62
Antonio Vicente Vento Maggio <antoniovicente.vento@schindler.com>
2021-09-29 11:59:58 +0000

    Updated SQL_Disks.ps1

--- scripts/Server_provisioning/SQL/SQL_Disks.ps1
+++ scripts/Server_provisioning/SQL/SQL_Disks.ps1
@@ -1,17 +1,20 @@
-Function SQL_Disk
-{
 Get-Disk | Where partitionstyle -eq ‘raw’ | Initialize-Disk -PartitionStyle GPT -PassThru
-New-Partition -AssignDriveLetter "2" -UseMaximumSize |
-Format-Volume -FileSystem NTFS -AllocationUnitSize 65536 -NewFileSystemLabel “SYSDB” -Confirm:$false
-New-Partition -AssignDriveLetter "3" -UseMaximumSize |
-Format-Volume -FileSystem NTFS -AllocationUnitSize 65536 -NewFileSystemLabel “USERDB” -Confirm:$false
-New-Partition -AssignDriveLetter "4" -UseMaximumSize |
-Format-Volume -FileSystem NTFS -AllocationUnitSize 65536 -NewFileSystemLabel “LOGDB” -Confirm:$false
-New-Partition -AssignDriveLetter "5" -UseMaximumSize |
-Format-Volume -FileSystem NTFS -AllocationUnitSize 65536 -NewFileSystemLabel “TEMPDB” -Confirm:$false
-}
-#####################################################################################
-SQL_Disk
+
+#Function SQL_Disk
+#{
+#
+#New-Partition -AssignDriveLetter "2" -UseMaximumSize |
+#Format-Volume -FileSystem NTFS -AllocationUnitSize 65536 -NewFileSystemLabel “SYSDB” -Confirm:$false
+#New-Partition -AssignDriveLetter "3" -UseMaximumSize |
+#Format-Volume -FileSystem NTFS -AllocationUnitSize 65536 -NewFileSystemLabel “USERDB” -Confirm:$false
+#New-Partition -AssignDriveLetter "4" -UseMaximumSize |
+#Format-Volume -FileSystem NTFS -AllocationUnitSize 65536 -NewFileSystemLabel “LOGDB” -Confirm:$false
+#New-Partition -AssignDriveLetter "5" -UseMaximumSize |
+#Format-Volume -FileSystem NTFS -AllocationUnitSize 65536 -NewFileSystemLabel “TEMPDB” -Confirm:$false
+#}
+######################################################################################
+#SQL_Disk
+
 
 #------------------------ Set in the Release inline script ------------------------
 #az account set --subscription "$(sub)"
