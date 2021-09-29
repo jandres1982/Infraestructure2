f4863d6de3fecf63642793999296695a7fd92aad
Antonio Vicente Vento Maggio <antoniovicente.vento@schindler.com>
2021-09-29 12:47:12 +0000

    Updated SQL_Disks.ps1

--- scripts/Server_provisioning/SQL/SQL_Disks.ps1
+++ scripts/Server_provisioning/SQL/SQL_Disks.ps1
@@ -1,4 +1,20 @@
-Get-Disk | Where partitionstyle -eq ‘raw’
+Initialize-Disk -Number "2" -PartitionStyle GPT
+Initialize-Disk -Number "3" -PartitionStyle GPT
+Initialize-Disk -Number "4" -PartitionStyle GPT
+Initialize-Disk -Number "5" -PartitionStyle GPT
+
+New-Partition -AssignDriveLetter "2" -UseMaximumSize |
+Format-Volume -FileSystem NTFS -AllocationUnitSize 65536 -NewFileSystemLabel “SYSDB” -Confirm:$false
+
+New-Partition -AssignDriveLetter "3" -UseMaximumSize |
+Format-Volume -FileSystem NTFS -AllocationUnitSize 65536 -NewFileSystemLabel “USERDB” -Confirm:$false
+
+New-Partition -AssignDriveLetter "4" -UseMaximumSize |
+Format-Volume -FileSystem NTFS -AllocationUnitSize 65536 -NewFileSystemLabel “LOGDB” -Confirm:$false
+
+New-Partition -AssignDriveLetter "5" -UseMaximumSize |
+Format-Volume -FileSystem NTFS -AllocationUnitSize 65536 -NewFileSystemLabel “TEMPDB” -Confirm:$false
+
 # Initialize-Disk -PartitionStyle GPT -PassThru
 
 #Function SQL_Disk
