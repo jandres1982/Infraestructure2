bb79c4c7498aa401cce2771f61b0b6a576292f27
Antonio Vento <antoniovicente.vento@schindler.com>
2021-09-29 13:36:45 +0200

    Disk_SQK_Fix.ps1

--- scripts/Server_provisioning/SQL/SQL_Disks.ps1
+++ scripts/Server_provisioning/SQL/SQL_Disks.ps1
@@ -1,12 +1,10 @@
 Function SQL_Disk
 {
-Get-Disk |            
-Where partitionstyle -eq ‘raw’ |           
-Initialize-Disk -PartitionStyle GPT -PassThru           
+Get-Disk | Where partitionstyle -eq ‘raw’ | Initialize-Disk -PartitionStyle GPT -PassThru
 New-Partition -AssignDriveLetter "2" -UseMaximumSize |
-Format-Volume -FileSystem NTFS -AllocationUnitSize 65536 -NewFileSystemLabel “SYSDB” -Confirm:$false           
+Format-Volume -FileSystem NTFS -AllocationUnitSize 65536 -NewFileSystemLabel “SYSDB” -Confirm:$false
 New-Partition -AssignDriveLetter "3" -UseMaximumSize |
-Format-Volume -FileSystem NTFS -AllocationUnitSize 65536 -NewFileSystemLabel “USERDB” -Confirm:$false            
+Format-Volume -FileSystem NTFS -AllocationUnitSize 65536 -NewFileSystemLabel “USERDB” -Confirm:$false
 New-Partition -AssignDriveLetter "4" -UseMaximumSize |
 Format-Volume -FileSystem NTFS -AllocationUnitSize 65536 -NewFileSystemLabel “LOGDB” -Confirm:$false
 New-Partition -AssignDriveLetter "5" -UseMaximumSize |
