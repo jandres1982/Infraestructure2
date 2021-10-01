d8a0d938f1dd5f06d4041135b4ad56e0c0783c31
Antonio Vento <antoniovicente.vento@schindler.com>
2021-10-01 14:16:00 +0200

    fix

--- scripts/automate-vm-status/Get-VM.ps1
+++ scripts/automate-vm-status/Get-VM.ps1
@@ -1,5 +1,5 @@
 Select-AzSubscription -Subscription "s-sis-eu-nonprod-01"
 $(get-azvm).name | where-object {$_ -like '*wsr*'}
 
-Select-AzSubscription -Subscription "s-sis-eu-nprod-01"
+Select-AzSubscription -Subscription "s-sis-eu-prod-01"
 $(get-azvm).name | where-object {$_ -like '*wsr*'}
