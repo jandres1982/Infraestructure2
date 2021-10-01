be045bdd610433b44db8522a55b308d8a8800221
Antonio Vento <antoniovicente.vento@schindler.com>
2021-10-01 14:20:48 +0200

    fix

--- scripts/automate-vm-status/Get-VM.ps1
+++ scripts/automate-vm-status/Get-VM.ps1
@@ -10,5 +10,5 @@ $VM_AP = $(get-azvm).name | where-object {$_ -like '*wsr*'}
 Select-AzSubscription -Subscription "s-sis-am-prod-01"
 $VM_AM_Prod = $(get-azvm).name | where-object {$_ -like '*wsr*'}
 
-Select-AzSubscription -Subscription "s-sis-am-nonprod-01
+Select-AzSubscription -Subscription "s-sis-am-nonprod-01"
 $VM_AM_NonProd = $(get-azvm).name | where-object {$_ -like '*wsr*'}
