f14371eb0b41ed5a078f0f238b98b24c51685d86
Antonio Vento <antoniovicente.vento@schindler.com>
2021-10-01 14:19:33 +0200

    Adding more Subs

--- scripts/automate-vm-status/Get-VM.ps1
+++ scripts/automate-vm-status/Get-VM.ps1
@@ -1,5 +1,14 @@
 Select-AzSubscription -Subscription "s-sis-eu-nonprod-01"
-$(get-azvm).name | where-object {$_ -like '*wsr*'}
+$VM_EU_NonProd = $(get-azvm).name | where-object {$_ -like '*wsr*'}
 
 Select-AzSubscription -Subscription "s-sis-eu-prod-01"
-$(get-azvm).name | where-object {$_ -like '*wsr*'}
+$VM_EU_Prod = $(get-azvm).name | where-object {$_ -like '*wsr*'}
+
+Select-AzSubscription -Subscription "s-sis-ap-prod-01"
+$VM_AP = $(get-azvm).name | where-object {$_ -like '*wsr*'}
+
+Select-AzSubscription -Subscription "s-sis-am-prod-01"
+$VM_AM_Prod = $(get-azvm).name | where-object {$_ -like '*wsr*'}
+
+Select-AzSubscription -Subscription "s-sis-am-nonprod-01
+$VM_AM_NonProd = $(get-azvm).name | where-object {$_ -like '*wsr*'}
