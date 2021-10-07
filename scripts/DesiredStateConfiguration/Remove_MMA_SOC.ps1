10afa26a708eb83f4ab32c547cd4d4652cd62f22
Antonio Vento <antoniovicente.vento@schindler.com>
2021-10-07 14:44:57 +0200

    DSC_MMA_EU

--- scripts/DesiredStateConfiguration/Remove_MMA_SOC.ps1
+++
@@ -1,6 +0,0 @@
-Write-Output "Working on Server"
-hostname
-$workspaceId = "b615f112-4439-41fa-aa80-424be76d309e"
-$mma = New-Object -ComObject 'AgentConfigManager.MgmtSvcCfg'
-$mma.RemoveCloudWorkspace($workspaceId)
-$mma.ReloadConfiguration()
