98c0a4c506b7e7b33791a6f962c683f8b4d01661
Antonio Vento <antoniovicente.vento@schindler.com>
2021-10-07 09:47:57 +0200

    Get-VM-Azure

--- scripts/automate-vm-status/Get-VM.ps1
+++ scripts/automate-vm-status/Get-VM.ps1
@@ -1,37 +1,66 @@
 
 
 
+#################################################################### EU prod
+Select-AzSubscription -Subscription "s-sis-eu-prod-01"
+$VM_EU_Prod = $(get-azvm).name | where-object {$_ -like '*wsr*'} > .\servers_list_eu_prod.txt
 
-##################################################################
-#Select-AzSubscription -Subscription "s-sis-eu-nonprod-01"
-#$VM_EU_NonProd = $(get-azvm).name | where-object {$_ -like '*wsr*'} > .\servers_list_eu_nonProd.txt
-#Select-AzSubscription -Subscription "s-sis-eu-prod-01"
-#$VM_EU_Prod = $(get-azvm).name | where-object {$_ -like '*wsr*'} > .\servers_list_eu_Prod.txt
-#Select-AzSubscription -Subscription "s-sis-ap-prod-01"
-#$VM_AP = $(get-azvm).name | where-object {$_ -like '*wsr*'} > .\servers_list_am_Prod.txt
-#Select-AzSubscription -Subscription "s-sis-am-prod-01"
-#$VM_AM_Prod = $(get-azvm).name | where-object {$_ -like '*wsr*'} > .\servers_list_ap.txt
-#Select-AzSubscription -Subscription "s-sis-am-nonprod-01"
-#$VM_AM_NonProd = $(get-azvm).name | where-object {$_ -like '*wsr*'} > .\servers_list_am_nonProd.txt
+$VM_EU_Prod = Get-Content "servers_list_eu_prod.txt"
+foreach ($vm in $VM_EU_Prod)
+{
+$rg = (get-azvm -Name $vm).ResourceGroupName
+write-host "$vm and $rg"
 
-
-####################################################################
-
+az vm run-command invoke --command-id RunPowerShellScript --name "$vm" -g $rg --scripts "hostname"
+}
+#################################################################### EU non prod
 Select-AzSubscription -Subscription "s-sis-eu-nonprod-01"
-$VM_EU_NonProd = $(get-azvm).name | where-object {$_ -like '*wsr*'} > .\servers_list_eu_nonProd.txt
+$VM_EU_NonProd = $(get-azvm).name | where-object {$_ -like '*wsr*'} > .\servers_list_eu_nonprod.txt
 
-$VM_EU_NonProd = Get-Content "servers_list_eu_nonProd.txt"
-
+$VM_EU_NonProd = Get-Content "servers_list_eu_nonprod.txt"
 foreach ($vm in $VM_EU_NonProd)
 {
 $rg = (get-azvm -Name $vm).ResourceGroupName
 write-host "$vm and $rg"
 
-#$app_id = "70eacc9c-bde8-4b40-9e16-02620fc4e65b"
-#$tenant_id = "aa06dce7-99d7-403b-8a08-0c5f50471e64"
-#$se_id = "e48bf748-b756-4b9a-ab72-1a2ee2bceb98"
-#az login --service-principal --username $app_id --password $se_id --tenant $tenant_id
-#az login -u $(user) -p $(pw)
-#Select-AzSubscription -Subscription "s-sis-eu-nonprod-01"
 az vm run-command invoke --command-id RunPowerShellScript --name "$vm" -g $rg --scripts "hostname"
 }
+
+#################################################################### Ap
+Select-AzSubscription -Subscription "s-sis-ap-prod-01"
+$VM_AP = $(get-azvm).name | where-object {$_ -like '*wsr*'} > .\servers_list_ap.txt
+$VM_AP = Get-Content "servers_list_ap.txt"
+foreach ($vm in $VM_AP)
+{
+$rg = (get-azvm -Name $vm).ResourceGroupName
+write-host "$vm and $rg"
+
+az vm run-command invoke --command-id RunPowerShellScript --name "$vm" -g $rg --scripts "hostname"
+}
+
+#################################################################### Am Prod
+Select-AzSubscription -Subscription "s-sis-am-prod-01"
+$VM_AM_Prod = $(get-azvm).name | where-object {$_ -like '*wsr*'} > .\servers_list_am_prod.txt
+$VM_AM_Prod = Get-Content "servers_list_am_prod.txt"
+foreach ($vm in $VM_AM_Prod)
+{
+$rg = (get-azvm -Name $vm).ResourceGroupName
+write-host "$vm and $rg"
+
+az vm run-command invoke --command-id RunPowerShellScript --name "$vm" -g $rg --scripts "hostname"
+}
+
+#################################################################### Am non prod
+Select-AzSubscription -Subscription "s-sis-am-nonprod-01"
+$VM_AM_NonProd = $(get-azvm).name | where-object {$_ -like '*wsr*'} > .\servers_list_am_nonprod.txt
+
+$VM_AM_NonProd = Get-Content "servers_list_am_nonprod.txt"
+foreach ($vm in $VM_AM_NonProd)
+{
+$rg = (get-azvm -Name $vm).ResourceGroupName
+write-host "$vm and $rg"
+
+az vm run-command invoke --command-id RunPowerShellScript --name "$vm" -g $rg --scripts "hostname"
+}
+
+#################################################################### 
