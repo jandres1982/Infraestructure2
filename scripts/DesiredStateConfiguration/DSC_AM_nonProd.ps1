8b5389b9e2e4194fe9aca6a24beeb2db77ba5b50
Antonio Vento <antoniovicente.vento@schindler.com>
2021-10-07 11:28:32 +0200

    check_remove_proxy

--- scripts/DesiredStateConfiguration/DSC_AM_nonProd.ps1
+++ scripts/DesiredStateConfiguration/DSC_AM_nonProd.ps1
@@ -14,15 +14,6 @@ az vm run-command invoke --command-id RunPowerShellScript --name "$vm" -g $rg --
 #Post_migration_task_Microsoft_Monitoring_Agent.
 
 
-function RemoveWorkID_MIG
-{
-$workspaceId = "a054b1bf-24eb-4e0b-a7e6-0fb782e77bf6"
-$mma = New-Object -ComObject 'AgentConfigManager.MgmtSvcCfg'
-$mma.RemoveCloudWorkspace($workspaceId)
-$mma.ReloadConfiguration()
-}
-
-
 function Remove_proxy
 {
 param($ProxyDomainName="webgateway-eu.schindler.com:3128")
@@ -40,42 +31,12 @@ $healthServiceSettings.SetProxyInfo('', '', '')
 $healthServiceSettings.SetProxyInfo("", "","")
 }
 
-function addWorkID_SoC
-{
-    $workspaceId = "b615f112-4439-41fa-aa80-424be76d309e"
-    $workspaceKey = "xO/JqiWFSYxGY7uIe1XgeFE3LjWFq8jvxoYyLcSGiHNkR/GnDG7eDd1WijUwMD7fW2y8rUnyLeVM8U1s9sDoqQ=="
-    $mma1 = New-Object -ComObject 'AgentConfigManager.MgmtSvcCfg'
-    $mma1.AddCloudWorkspace($workspaceId, $workspaceKey)
-    $mma1.ReloadConfiguration()
-}
-
-
-function addWorkID_SCC
-{
-    $workspaceId = "fa488d5a-d8e4-4437-9ccc-2ef59e9eb669"
-    $workspaceKey = "1DxbXeHBAM3QLWl4GcE9SF0eTCEYuyr5pAt5k3wGG+bASH/ug9XGmVUyHKGvi/nmVIAYLLvfemwkuhM0yxGWCA=="
-    $mma1 = New-Object -ComObject 'AgentConfigManager.MgmtSvcCfg'
-    $mma1.AddCloudWorkspace($workspaceId, $workspaceKey)
-    $mma1.ReloadConfiguration()
-}
-
-
-Function Check_WID_Provided
-{
-$mma = New-Object -ComObject 'AgentConfigManager.MgmtSvcCfg'
-$mma.GetCloudWorkspaces()
-$WID = $mma.GetCloudWorkspaces()
-}
 
 #main
 
-RemoveWorkID_MIG
-
 Remove_proxy
 
-addWorkID_SCC
 
-Check_WID_Provided
 
 "
 }
