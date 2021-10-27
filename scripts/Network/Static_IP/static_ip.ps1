4e9dab7fb789866ecd854612a589149856a426c3
Antonio Vicente Vento Maggio <antoniovicente.vento@schindler.com>
2021-10-27 08:07:03 +0000

    Added static_ip.ps1

---
+++ scripts/Network/Static_IP/static_ip.ps1
@@ -0,0 +1,15 @@
+Set-AzContext -Subscription s-sis-am-nonprod-01
+
+$nics = (Get-AzNetworkInterface | Where-Object {$_.IpConfigurations.PrivateIpAllocationMethod -eq 'dynamic'} | Where-Object {$_.VirtualMachine.id -like '*wsr*'}).Name > "D:\Repository\Working\Antonio\Set_Ip_Static\nics_dynamic_am_nonprod.txt"
+$nics = Get-Content "D:\Repository\Working\Antonio\Set_Ip_Static\nics_dynamic_am_nonprod.txt"
+foreach ($nic in $nics)
+{
+Write-Host $nic
+$nic_full = Get-AzNetworkInterface -name $nic
+$ip = $(Get-AzNetworkInterface -Name $nic).IpConfigurations.PrivateIpAddress
+$config_name = $(Get-AzNetworkInterface -Name $nic).IpConfigurations.name
+$subnet = $(Get-AzNetworkInterface -Name $nic).IpConfigurations.subnet.id
+$nic_full | Set-AzNetworkInterfaceIpConfig -Name $config_name -PrivateIpAddress $ip -SubnetId $subnet
+$nic_full | Set-AzNetworkInterface
+
+}
