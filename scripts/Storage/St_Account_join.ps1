124bf6326be62de1d32c279d013984114710170c
Antonio Vento <antoniovicente.vento@schindler.com>
2021-10-28 13:19:36 +0200

    ST accunt

--- scripts/Storage/St_Account_join.ps1
+++ scripts/Storage/St_Account_join.ps1
@@ -5,3 +5,6 @@ $Domain = "global.schindler.com"
 $OU = "OU=zzz,OU=servers,OU=NBI12,DC=global,DC=schindler,DC=com"
 $ResourceGroupName = "rg-cis-nonprod-storage-01"
 Join-AzStorageAccountForAuth -ResourceGroupName $ResourceGroupName -StorageAccountName $Storage_account -DomainAccountType ComputerAccount -OrganizationalUnitDistinguishedName $OU
+
+azcopy copy "C:\local\path" "https://account.blob.core.windows.net/mycontainer1/?sv=2018-03-28&ss=bjqt&srt=sco&sp=rwddgcup&se=2019-05-01T05:01:17Z&st=2019-04-30T21:01:17Z&spr=https&sig=MGCXiyEzbtttkr3ewJIh2AR8KrghSy1DGM9ovN734bQF4%3D" --recursive=true –preserve-smb-permissions=true
+
