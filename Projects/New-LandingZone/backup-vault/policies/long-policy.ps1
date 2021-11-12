c1e959d7de18618012083d06845422fa3740ca89
Nahum Sancho <nahum.sancho@schindler.com>
2021-11-12 09:02:52 +0100

    Vault policies update

---
+++ Projects/New-LandingZone/backup-vault/policies/long-policy.ps1
@@ -0,0 +1,5 @@
+$Vault = Get-AzureRmBackupVault -Name "rsv-nonprod-chno-lrsbackup-01"
+$Daily = New-AzureRmBackupRetentionPolicyObject -DailyRetention -Retention 30
+$Weekly = New-AzureRmBackupRetentionPolicyObject --Weekly -DaysOfWeek (sunday) -Retention 12
+$Monthly = New-AzureRmBackupRetentionPolicyObject -MonthlyRetentionInDailyFormat -DaysOfMonth (1) -Retention 12
+$ProtectionPolicy = New-AzureRmBackupProtectionPolicy -Name "vm-short-01am-01" -Type AzureVM -Daily -BackupTime ([datetime]"1:00 AM") -RetentionPolicy ($Daily,$Weekly) -Vault $Vault
