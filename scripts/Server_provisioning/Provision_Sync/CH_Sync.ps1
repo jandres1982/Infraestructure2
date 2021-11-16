dd18ebd86fa9d6153663d10e5bd2090ba083f60f
Antonio Vicente Vento Maggio <antoniovicente.vento@schindler.com>
2021-11-16 14:55:12 +0000

    Updated CH_Sync.ps1

--- scripts/Server_provisioning/Provision_Sync/CH_Sync.ps1
+++ scripts/Server_provisioning/Provision_Sync/CH_Sync.ps1
@@ -4,9 +4,9 @@ $log = "c:\temp\provision_sync_CH.txt"
 $connectTestResult = Test-NetConnection -ComputerName 10.44.1.4 -Port 445
 if ($connectTestResult.TcpTestSucceeded) {
     # Save the password so the drive will persist on reboot
-    cmd.exe /C "cmdkey /add:`"10.44.1.4`" /user:`"Azure\stprodgeneric0001`" /pass:`"U7JZ4rXAfdk10d5RwkDbpC4xnhrFRW3Fnx3DJdLCN3puRBadRIyXyHZ/7e5inv3NUKOfwhiRzwELVkh5USI2Fg==`""
+    cmd.exe /C "cmdkey /add:`"10.44.1.4`" /user:`"localhost\stprodgeneric0003`" /pass:`"958MZdkgvmry4X5F8WUpqJlJXviSkodcIQ1KP34ONmYHCurfk9ZfonOQ3Z7AXrpAbfq4ET6a0EiTgotxvB9i/w==`""
     # Mount the drive
-    New-PSDrive -Name R -PSProvider FileSystem -Root "\\10.44.1.4\servers"
+    New-PSDrive -Name R -PSProvider FileSystem -Root "\\10.44.1.4\servers" -Persist
 } else {
     Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
 }
