0fd6e3e672c03b88df3312ce5c6fbc530d2299d3
Antonio Vicente Vento Maggio <antoniovicente.vento@schindler.com>
2021-11-17 07:54:41 +0000

    $per

--- scripts/DesiredStateConfiguration/DSC_EU_nonProd.ps1
+++ scripts/DesiredStateConfiguration/DSC_EU_nonProd.ps1
@@ -1,12 +1,40 @@
 
+
 #################################################################### EU non prod
+
+
 Select-AzSubscription -Subscription "s-sis-eu-nonprod-01"
 $VM_EU_NonProd = $(get-azvm).name | where-object {$_ -like '*wsr*'} > .\servers_list_eu_nonprod.txt
-
 $VM_EU_NonProd = Get-Content "servers_list_eu_nonprod.txt"
+
 foreach ($vm in $VM_EU_NonProd)
 {
+
+
+##########################  Check $per
+[int]$num_T = $VM_EU_NonProd.Count
+[int]$num_R = $num_T
+[int]$Per = $null
+[int]$Per_1 = $null
+$num_R = $num_R - 1
+$Per = 100 - (($num_R * 100) / $num_T)
+
+if ($per -eq $per_1)
+{
+#write-host "is equal"
+}else
+{
+Write-host "Servers checked $per%"
+}
+$per_1 = $per
+}
+
+##########################  Check $per
+
 $rg = (get-azvm -Name $vm).ResourceGroupName
 write-host "$vm and $rg"
 az vm run-command invoke --command-id RunPowerShellScript --name "$vm" -g $rg --scripts "@DSC_MMA.ps1"
+
+
+
 }
