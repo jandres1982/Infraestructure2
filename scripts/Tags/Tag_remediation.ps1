35153907ecd7d95e7170da43f3cac3d6dfa17159
Antonio Vento <antoniovicente.vento@schindler.com>
2021-11-23 16:45:29 +0100

    Tag_remediation

---
+++ scripts/Tags/Tag_remediation.ps1
@@ -0,0 +1,14 @@
+$Resource_group = Get-content "C:\Users\ventoa1\OneDrive - Schindler\Azure_Devops\Infraestructure\scripts\Tags\rg.txt"
+
+foreach ($rg in $Resource_group) {
+    $tags = $(Get-AzResourceGroup -Name $rg).Tags
+        $Resource_id = $(Get-AzResource -ResourceGroupName $rg).id
+        #$resource_id = "/subscriptions/7fa3c3a2-7d0d-4987-a30c-30623e38756c/resourceGroups/RG-GIS-TEST-SAPHANAPOC-01/providers/Microsoft.Compute/virtualMachines/shhlsr8003"
+        foreach ($rid in $Resource_id) {
+            Update-AzTag -ResourceId $rid -Tag $Tags -Operation Merge
+        }
+}
+
+#/subscriptions/7fa3c3a2-7d0d-4987-a30c-30623e38756c/resourceGroups/RG-GIS-TEST-SAPHANAPOC-01/providers/Microsoft.Compute/disks/shhlsr4012_disk_os
+
+#$tags_resource = $(Get-AzResource -id $resources_id).Tags
