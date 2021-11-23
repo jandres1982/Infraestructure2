2bfeb3b9943ba21db5c7254535947fc1fcfd7aef
Nahum Sancho Puri <nahum.sancho@schindler.com>
2021-11-23 06:42:28 +0000

    Deleted New-LandingZone

--- Projects/New-LandingZone/backup-vault/policies/short-policy.ps1
+++
@@ -1,3 +0,0 @@
-$Vault = Get-AzureRmBackupVault -Name "rsv-nonprod-chno-lrsbackup-01"
-$Daily = New-AzureRmBackupRetentionPolicyObject -DailyRetention -Retention 30
-$ProtectionPolicy = New-AzureRmBackupProtectionPolicy -Name "vm-short-01am-01" -Type AzureVM -Daily -BackupTime ([datetime]"1:00 AM") -RetentionPolicy ($Daily) -Vault $Vault
