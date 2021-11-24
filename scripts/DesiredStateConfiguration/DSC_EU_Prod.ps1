071fc907d772bf6d22689d17dfcfc9317c14d9af
Antonio Vento <antoniovicente.vento@schindler.com>
2021-11-24 16:33:46 +0100

    DSC prod

--- scripts/DesiredStateConfiguration/DSC_EU_Prod.ps1
+++ scripts/DesiredStateConfiguration/DSC_EU_Prod.ps1
@@ -1,12 +1,18 @@
 #################################################################### EU prod
 Select-AzSubscription -Subscription "s-sis-eu-prod-01"
-$VM_EU_Prod = $(get-azvm).name | where-object {$_ -like '*wsr*'} > .\servers_list_eu_prod.txt
 
+workflow DSC
+{
+
+$VM_EU_Prod = $(get-azvm).name | where-object {$_ -like '*wsr*'} > .\servers_list_eu_prod.txt
 $VM_EU_Prod = Get-Content "servers_list_eu_prod.txt"
-foreach ($vm in $VM_EU_Prod)
+
+
+foreach -parallel ($vm in $VM_EU_Prod)
 {
 $rg = (get-azvm -Name $vm).ResourceGroupName
 write-host "$vm and $rg"
 
 az vm run-command invoke --command-id RunPowerShellScript --name "$vm" -g $rg --scripts "@DSC_MMA.ps1"
+}
 }
