4ed404d48e0875a8821042d447d4800ba17f976f
Antonio Vicente Vento Maggio <antoniovicente.vento@schindler.com>
2021-12-01 11:45:02 +0000

    Updated Defender.ps1

--- scripts/Server_provisioning/Install_packages/Defender.ps1
+++ scripts/Server_provisioning/Install_packages/Defender.ps1
@@ -1,5 +1,17 @@
 $OSVersion = (Get-WMIObject win32_operatingsystem).caption
 If ($OSVersion -like '*2019*')
 {
-cmd.exe /c "c:\provision\Microsoft\Defender\WindowsDefenderATPOnboardingScript.cmd"
+cmd.exe /c "c:\provision\Microsoft\Defender\2019\WindowsDefenderATPOnboardingScript.cmd"
+}
+
+$OSVersion = (Get-WMIObject win32_operatingsystem).caption
+If ($OSVersion -like '*2016*')
+{
+#cmd.exe /c "c:\provision\Microsoft\Defender\2012-2016\WindowsDefenderATPOnboardingScript.cmd"
+}
+
+$OSVersion = (Get-WMIObject win32_operatingsystem).caption
+If ($OSVersion -like '*2012*')
+{
+#cmd.exe /c "c:\provision\Microsoft\Defender\2012-2016\WindowsDefenderATPOnboardingScript.cmd"
 }
