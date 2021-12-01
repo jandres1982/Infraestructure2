97d292cc586b625d0c7e655a3fa0a3c028b5853e
Antonio Vicente Vento Maggio <antoniovicente.vento@schindler.com>
2021-12-01 15:55:55 +0000

    Updated Set_PF_T.ps1

--- scripts/Server_provisioning/Install_packages/Set_PF_T.ps1
+++ scripts/Server_provisioning/Install_packages/Set_PF_T.ps1
@@ -1,5 +1,10 @@
 $Temp_Disk = Get-volume | Where-Object -Property FileSystemLabel  -eq -Value "Temporary Storage"
+if ($Temp_Disk)
+{
 $Letter = $Temp_Disk.DriveLetter
 Get-Partition -DriveLetter $Letter | Set-Partition -NewDriveLetter T
 Set-Itemproperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management' -Name 'PagingFiles' -value 'T:\pagefile.sys 0 0'
 restart-computer -force
+}else
+{Write-Output "Temporary Storage Found"
+}
