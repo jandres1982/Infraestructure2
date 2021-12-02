7ba3fbe9d6bdcd3e26ddfe0d4b2f859220900050
Antonio <azdevopsantonio1707@outlook.com>
2021-12-02 09:09:26 +0100

    Defender changed to AntiVirus

---
+++ scripts/Server_provisioning/Install_packages/AntiVirus.ps1
@@ -0,0 +1,17 @@
+$OSVersion = (Get-WMIObject win32_operatingsystem).caption
+If ($OSVersion -like '*2019*')
+{
+cmd.exe /c "c:\provision\Microsoft\Defender\2019\WindowsDefenderATPOnboardingScript.cmd"
+}
+
+$OSVersion = (Get-WMIObject win32_operatingsystem).caption
+If ($OSVersion -like '*2016*')
+{
+cmd.exe /c "msiexec.exe /i c:\provision\symantec\SEP_14.3.558.0000\Sep64.msi" /qn SYMREBOOT=ReallySuppress
+}
+
+$OSVersion = (Get-WMIObject win32_operatingsystem).caption
+If ($OSVersion -like '*2012*')
+{
+cmd.exe /c "msiexec.exe /i c:\provision\symantec\SEP_14.3.558.0000\Sep64.msi" /qn SYMREBOOT=ReallySuppress
+}
