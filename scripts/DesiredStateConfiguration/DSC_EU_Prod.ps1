3c927790152317ca39744e92c119770b23d16b24
Antonio Vento <antoniovicente.vento@schindler.com>
2021-12-20 15:54:41 +0100

    ok

--- scripts/DesiredStateConfiguration/DSC_EU_Prod.ps1
+++ scripts/DesiredStateConfiguration/DSC_EU_Prod.ps1
@@ -19,7 +19,23 @@ write-output "MicrosoftMonitoringAgent extension found in the server"
 }else
 {
     write-output "MicrosoftMonitoringAgent Agent not found"
-    az vm extension set -n MicrosoftMonitoringAgent --publisher Microsoft.EnterpriseCloud.Monitoring --version 1.0 --vm-name "$vm" --resource-group "$rg"
+    
+    write-output "We will try to install the agent"
+
+
+    $PublicSettings = @{"workspaceId" = "fa488d5a-d8e4-4437-9ccc-2ef59e9eb669"}
+    $ProtectedSettings = @{"workspaceKey" = "1DxbXeHBAM3QLWl4GcE9SF0eTCEYuyr5pAt5k3wGG+bASH/ug9XGmVUyHKGvi/nmVIAYLLvfemwkuhM0yxGWCA=="}
+
+    Set-AzVMExtension -ExtensionName "MicrosoftMonitoringAgent" `
+    -ResourceGroupName "$rg" `
+    -VMName "$vm" `
+    -Publisher "Microsoft.EnterpriseCloud.Monitoring" `
+    -ExtensionType "MicrosoftMonitoringAgent" `
+    -TypeHandlerVersion 1.0 `
+    -Settings $PublicSettings `
+    -ProtectedSettings $ProtectedSettings `
+    -Location NorthEurope
+
 }
 #az account set --subscription <subscription-id>
 ################################################################################################
