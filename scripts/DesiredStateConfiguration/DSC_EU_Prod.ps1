32a29a8d645519c2dc6888dbaea18dc083adaba0
Antonio Vento <antoniovicente.vento@schindler.com>
2021-12-20 12:45:03 +0100

    ok

--- scripts/DesiredStateConfiguration/DSC_EU_Prod.ps1
+++ scripts/DesiredStateConfiguration/DSC_EU_Prod.ps1
@@ -2,6 +2,26 @@
 Select-AzSubscription -Subscription "s-sis-eu-prod-01"
 
 $(get-azvm).name | where-object {$_ -like '*wsr*'} > .\servers_list_eu_prod.txt
+
+######################### Check MicrosoftMonitoringAgent extension is enable in the VM 
+
+$extension = $(Get-AzVM -ResourceGroupName "RG-POR-PROD-FILESERVER-01" -Name "porwsr0014" -DisplayHint expand).extensions.name | Where-Object {$_ -eq "MicrosoftMonitoringAgent"}
+if ($extension -eq "MicrosoftMonitoringAgent")
+{
+write-output "MicrosoftMonitoringAgent extension found in the server"
+}else
+{
+    write-output "MicrosoftMonitoringAgent Agent not found"
+}
+
+
+
+
+
+
+################################################################################################
+
+
 $VM_EU_Prod  = Get-Content "servers_list_eu_prod.txt"
 foreach ($vm in $VM_EU_Prod)
 {
