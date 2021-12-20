f99613c0547b633b722f98f4ef4ea0800dd20fe0
Antonio Vento <antoniovicente.vento@schindler.com>
2021-12-20 15:04:29 +0100

    DSC_EU_prod_servers

--- scripts/DesiredStateConfiguration/DSC_EU_Prod.ps1
+++ scripts/DesiredStateConfiguration/DSC_EU_Prod.ps1
@@ -3,29 +3,133 @@ Select-AzSubscription -Subscription "s-sis-eu-prod-01"
 
 $(get-azvm).name | where-object {$_ -like '*wsr*'} > .\servers_list_eu_prod.txt
 
-######################### Check MicrosoftMonitoringAgent extension is enable in the VM 
-
-$extension = $(Get-AzVM -ResourceGroupName "RG-POR-PROD-FILESERVER-01" -Name "porwsr0014" -DisplayHint expand).extensions.name | Where-Object {$_ -eq "MicrosoftMonitoringAgent"}
-if ($extension -eq "MicrosoftMonitoringAgent")
-{
-write-output "MicrosoftMonitoringAgent extension found in the server"
-}else
-{
-    write-output "MicrosoftMonitoringAgent Agent not found"
-}
-
-
-
-
-
-
-################################################################################################
-
 
 $VM_EU_Prod  = Get-Content "servers_list_eu_prod.txt"
 foreach ($vm in $VM_EU_Prod)
 {
 $rg = (get-azvm -Name $vm).ResourceGroupName
 write-host "$vm and $rg"
+
+######################### Check MicrosoftMonitoringAgent extension is enable in the VM 
+
+$extension = $(Get-AzVM -ResourceGroupName "$rg" -Name "$vm" -DisplayHint expand).extensions.name | Where-Object {$_ -eq "MicrosoftMonitoringAgent"}
+if ($extension -eq "MicrosoftMonitoringAgent")
+{
+write-output "MicrosoftMonitoringAgent extension found in the server"
+}else
+{
+    write-output "MicrosoftMonitoringAgent Agent not found"
+    az vm extension set -n MicrosoftMonitoringAgent --publisher Microsoft.EnterpriseCloud.Monitoring --version 1.0 --vm-name "$vm" --resource-group "rg"
+}
+#az account set --subscription <subscription-id>
+################################################################################################
+
+
+
 az vm run-command invoke --command-id RunPowerShellScript --name "$vm" -g $rg --scripts "@DSC_MMA.ps1"
 }
+
+
+
+
+az vm extension set -n Microsoft.Insights.LogAnalyticsAgent --publisher Microsoft.EnterpriseCloud.Monitoring --version 1.0 --vm-name "zzzwsr0010" --resource-group MyResourceGroup "rg-cis-test-server-01"
+
+
+#PS /home/antonio> az vm extension list -g "rg-cis-test-server-01" --vm-name "zzzwsr0010"
+#[
+#  {
+#    "autoUpgradeMinorVersion": true,
+#    "enableAutomaticUpgrade": null,
+#    "forceUpdateTag": null,
+#    "id": "/subscriptions/7fa3c3a2-7d0d-4987-a30c-30623e38756c/resourceGroups/rg-cis-test-server-01/providers/Microsoft.Compute/virtualMachines/zzzwsr0010/extensions/Microsoft.Insights.LogAnalyticsAgent",
+#    "instanceView": null,
+#    "location": "northeurope",
+#    "name": "Microsoft.Insights.LogAnalyticsAgent",
+#    "protectedSettings": null,
+#    "provisioningState": "Succeeded",
+#    "publisher": "Microsoft.EnterpriseCloud.Monitoring",
+#    "resourceGroup": "rg-cis-test-server-01",
+#    "settings": {
+#      "workspaceId": "fa488d5a-d8e4-4437-9ccc-2ef59e9eb669"
+#    },
+#    "suppressFailures": null,
+#    "tags": {
+#      "applicationowner": "alfonso.marques@schindler.com",
+#      "costcenter": "120011300",
+#      "infrastructureservice": "si-server-hardware-operating-system",
+#      "kg": "sis",
+#      "serviceowner": "teodora.iliuta@schindler.com",
+#      "technicalcontact": "alfonso.marques@schindler.com"
+#    },
+#    "type": "Microsoft.Compute/virtualMachines/extensions",
+#    "typeHandlerVersion": "1.0",
+#    "typePropertiesType": "MicrosoftMonitoringAgent"
+#  }
+#]
+
+
+
+#PS /home/antonio> az vm extension list -g "rg-par-prod-outputmgmt-01" --vm-name "parwsr0108"
+#[
+#  {
+#    "autoUpgradeMinorVersion": true,
+#    "enableAutomaticUpgrade": null,
+#    "forceUpdateTag": null,
+#    "id": "/subscriptions/505ead1a-5a5f-4363-9b72-83eb2234a43d/resourceGroups/rg-par-prod-outputmgmt-01/providers/Microsoft.Compute/virtualMachines/parwsr0108/extensions/MicrosoftMonitoringAgent",
+#    "instanceView": null,
+#    "location": "northeurope",
+#    "name": "MicrosoftMonitoringAgent",
+#    "protectedSettings": null,
+#    "provisioningState": "Succeeded",
+#    "publisher": "Microsoft.EnterpriseCloud.Monitoring",
+#    "resourceGroup": "rg-par-prod-outputmgmt-01",
+#    "settings": {
+#      "workspaceId": "fa488d5a-d8e4-4437-9ccc-2ef59e9eb669"
+#    },
+#    "suppressFailures": null,
+#    "tags": {
+#      "applicationowner": "brigitte.kingue.priso@schindler.com",
+#      "costcenter": "120036060",
+#      "infrastructureservice": "outputmgmt",
+#      "kg": "par",
+#      "serviceowner": "brigitte.kingue.priso@schindler.com",
+#      "technicalcontact": "serge.rollin@schindler.com"
+#    },
+#    "type": "Microsoft.Compute/virtualMachines/extensions",
+#    "typeHandlerVersion": "1.0",
+#    "typePropertiesType": "MicrosoftMonitoringAgent"
+#  },
+#  {
+#    "autoUpgradeMinorVersion": true,
+#    "enableAutomaticUpgrade": null,
+#    "forceUpdateTag": null,
+#    "id": "/subscriptions/505ead1a-5a5f-4363-9b72-83eb2234a43d/resourceGroups/rg-par-prod-outputmgmt-01/providers/Microsoft.Compute/virtualMachines/parwsr0108/extensions/SqlIaasExtension",
+#    "instanceView": null,
+#    "location": "northeurope",
+#    "name": "SqlIaasExtension",
+#    "protectedSettings": null,
+#    "provisioningState": "Failed",
+#    "publisher": "Microsoft.SqlServer.Management",
+#    "resourceGroup": "rg-par-prod-outputmgmt-01",
+#    "settings": {
+#      "DeploymentTokenSettings": {
+#        "DeploymentToken": 1752625104
+#      },
+#      "SqlManagement": {
+#        "IsEnabled": false
+#      }
+#    },
+#    "suppressFailures": null,
+#    "tags": {
+#      "applicationowner": "brigitte.kingue.priso@schindler.com",
+#      "costcenter": "120036060",
+#      "infrastructureservice": "outputmgmt",
+#      "kg": "par",
+#      "serviceowner": "brigitte.kingue.priso@schindler.com",
+#      "technicalcontact": "serge.rollin@schindler.com"
+#    },
+#    "type": "Microsoft.Compute/virtualMachines/extensions",
+#    "typeHandlerVersion": "2.0",
+#    "typePropertiesType": "SqlIaaSAgent"
+#  }
+#]
