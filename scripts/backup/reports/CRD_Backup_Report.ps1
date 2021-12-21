a4dd72f3fe101840dc95e2bb2c68dc822a9d9fac
Antonio Vento <antoniovicente.vento@schindler.com>
2021-12-21 13:10:47 +0100

    CRD all sub Report

--- scripts/backup/reports/CRD_Backup_Report.ps1
+++ scripts/backup/reports/CRD_Backup_Report.ps1
@@ -1,4 +1,11 @@
-Select-AzSubscription -Subscription "s-sis-eu-prod-01"
+$subs = @("s-sis-eu-nonprod-01","s-sis-eu-prod-01","s-sis-am-prod-01","s-sis-am-nonprod-01","s-sis-ap-prod-01")
+
+###################################################################
+
+foreach ($sub in $subs)
+{
+
+Select-AzSubscription -Subscription "$sub"
 $date = $(get-date -format yyyy-MM-ddTHH-mm)
 $Vault_List = (Get-AzRecoveryServicesVault).Name 
 foreach ($vault in $Vault_List) 
@@ -9,7 +16,7 @@ $Vault_ResourecGroup = (Get-AzRecoveryServicesVault -Name $vault).ResourceGroupN
 $Vault_ID = Get-AzRecoveryServicesVault -ResourceGroupName "$Vault_ResourecGroup" -Name $vault 
 $Jobs = Get-AzRecoveryServicesBackupJob -VaultId $Vault_ID.Id | where-object {$_.WorkLoadname -like "*crdwsr*"}
 #$JobDetails = Get-AzRecoveryServicesBackupJobDetail -Job $Jobs[0] -VaultId $vault_ID.ID | Export-Csv -Path "Backup_Report_CRD_Prod_$date.csv"  -Append 
-$Jobs | Export-Csv -Path "Backup_Report_CRD_Prod_$date.csv" -Append -Force 
+$Jobs | Export-Csv -Path "Backup_Report_CRD_$date.csv" -Append -Force 
 } 
 
 
@@ -17,9 +24,9 @@ $PSEmailServer = "smtp.eu.schindler.com"
 $From = "scc-support-zar.es@schindler.com"
 $to = "antoniovicente.vento@schindler.com","alfonso.marques@schindler.com"
 
-$Subject = "Backup Report CRD Prod Subscription"
+$Subject = "Backup Report CRD Servers"
 #$Filename = Get-ChildItem $Path -Name "Att*" | select -Last 1
-$Attachment = "Backup_Report_CRD_Prod_$date.csv"
+$Attachment = "Backup_Report_CRD_$date.csv"
 $Body = @"
 Dear team,
 
@@ -32,3 +39,5 @@ Report saved in the Server Team Scripting Server
 Send-MailMessage -From $From -To $To -Subject $Subject -Body $Body -Attachments $Attachment
 
 Copy-Item -Path $Attachment -Destination \\shhwsr1849\Backup_Report_Azure -Force
+
+}
