0f9a1c828e9c2dbc99776ec0e65fc35379456bbc
Antonio Vento <antoniovicente.vento@schindler.com>
2021-12-21 11:09:33 +0100

    Null to command

--- scripts/DesiredStateConfiguration/DSC_EU_Prod.ps1
+++ scripts/DesiredStateConfiguration/DSC_EU_Prod.ps1
@@ -43,22 +43,15 @@ if ($extension -eq "MicrosoftMonitoringAgent")
         $status = "Microsoft.Insights.LogAnalyticsAgent"
         }else #no MMA agent Found
              {       
-              $status = "MicrosoftMonitoringAgent Installation" 
+              $status = "ALERT! Installation of MicrosoftMonitoringAgent" 
               #write-output "MMA Agent not found, pushing installation"
               
               
               $PublicSettings = @{"workspaceId" = "fa488d5a-d8e4-4437-9ccc-2ef59e9eb669"}
               $ProtectedSettings = @{"workspaceKey" = "1DxbXeHBAM3QLWl4GcE9SF0eTCEYuyr5pAt5k3wGG+bASH/ug9XGmVUyHKGvi/nmVIAYLLvfemwkuhM0yxGWCA=="}
               
-              Set-AzVMExtension -ExtensionName "MicrosoftMonitoringAgent" `
-              -ResourceGroupName "$rg" `
-              -VMName "$vm" `
-              -Publisher "Microsoft.EnterpriseCloud.Monitoring" `
-              -ExtensionType "MicrosoftMonitoringAgent" `
-              -TypeHandlerVersion 1.0 `
-              -Settings $PublicSettings `
-              -ProtectedSettings $ProtectedSettings `
-              -Location NorthEurope
+              Set-AzVMExtension -ExtensionName "MicrosoftMonitoringAgent" -ResourceGroupName "$rg" -VMName "$vm" -Publisher "Microsoft.EnterpriseCloud.Monitoring" -ExtensionType "MicrosoftMonitoringAgent" -TypeHandlerVersion 1.0 -Settings $PublicSettings -ProtectedSettings $ProtectedSettings -Location NorthEurope > $null
+              
               }
 
         }
