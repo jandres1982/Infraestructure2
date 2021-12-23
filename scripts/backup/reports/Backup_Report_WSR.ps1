9d19160310c4f9f739ec647868cd7b3cea17b79d
Antonio Vento <antoniovicente.vento@schindler.com>
2021-12-23 12:41:43 +0100

    unique and sorted

--- scripts/backup/reports/Backup_Report_WSR.ps1
+++ scripts/backup/reports/Backup_Report_WSR.ps1
@@ -20,7 +20,7 @@ $Jobs = Get-AzRecoveryServicesBackupJob -VaultId $Vault_ID.Id -BackupManagementT
 #$JobDetails = Get-AzRecoveryServicesBackupJobDetail -Job $Jobs[0] -VaultId $vault_ID.ID | Export-Csv -Path "Backup_Report_CRD_Prod_$date.csv"  -Append 
 $Jobs | Export-Csv -Path "Backup_Report_WSR_$date.csv" -Append -Force 
 
-$Jobs_failed = Get-AzRecoveryServicesBackupJob -From (Get-Date).AddDays(-3).ToUniversalTime() -Status Failed -VaultId $Vault_ID.id
+$Jobs_failed = Get-AzRecoveryServicesBackupJob -From (Get-Date).AddDays(-3).ToUniversalTime() -Status Failed -VaultId $Vault_ID.id |  Sort-Object -Property @{Expression={$_.WorkloadName}} -Unique | Where-Object {$_.WorkloadName -like "*wsr*"}
 $Jobs_failed | Export-Csv -Path "Backup_Report_WSR_FAILED_$date.csv" -Append -Force
 
 }
