34a8632834c707b4eebf87ea0e903c504e1ed570
Antonio Vento <antoniovicente.vento@schindler.com>
2021-12-23 09:56:11 +0100

    Local admin group for DMZ

---
+++ scripts/Server_provisioning/Install_packages/Local_Admin_Group_Task_DMZ.ps1
@@ -0,0 +1,17 @@
+$date = Get-Date
+$dt = $date.AddMinutes(1)
+$hostname = hostname
+$Action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument "-File C:\provision\Schindler\add_admin_group\Add_Admin_Group_Dmz.ps1"
+$Trigger = New-ScheduledTaskTrigger -Once -At $dt
+$Settings = New-ScheduledTaskSettingsSet
+$taskname = 'Local Admin Group'
+Register-ScheduledTask -TaskName $taskname `
+                       -TaskPath "\" `
+                       -Action $Action `
+                       -User 'ldmsosd' `
+                       -Password 'Newsetup1234' `
+                       -Trigger $trigger `
+                       -Settings $Settings `
+                       -RunLevel Highest -Force
+start-sleep 3
+Start-ScheduledTask -TaskName 'Local Admin Group'
