4848b3e724a9be74f78677220392d34050ed66e7
Nahum Sancho <nahum.sancho@schindler.com>
2022-02-01 11:41:53 +0100

    New vault backup v1.2

--- scripts/backup/new-vault.ps1
+++ scripts/backup/new-vault.ps1
@@ -2,6 +2,9 @@
 $vaultname="rsv-nonprod-use2-lrsbackupsql-01"
 $rg="rg-cis-nonprod-backup-01"
 $location="eastus2"
+$sub="s-sis-am-nonprod-01"
+### Select the subscription ###
+Set-AzContext -Subscription $sub
 ### Creating recovery service vault ###
 New-AzRecoveryServicesVault -Name $vaultname -ResourceGroupName $rg -Location $location
 ### Store vault properties in a new variable ##
@@ -10,3 +13,8 @@ $vault=Get-AzRecoveryServicesVault -Name $vaultname
 Set-AzRecoveryServicesBackupProperty -Vault $vault -BackupStorageRedundancy LocallyRedundant
 ### Enable identity for the vault ###
 Update-AzRecoveryServicesVault -ResourceGroupName $vault.ResourceGroupName -Name $vault.Name -IdentityType SystemAssigned
+### Store managed identity id ###
+$managedidentity= Get-AzADServicePrincipal -DisplayName $vaultname
+### Grant Contributor Role over RG for Vault Identity ###
+New-AzRoleAssignment -ObjectId $managedidentity.id -RoleDefinitionName "Contributor" -ResourceGroupName $rg
+#New-AzRoleAssignment -ObjectId $managedidentity.id -RoleDefinitionName "Contributor" -ResourceName Devices-Engineering-ProjectRND -ResourceType Microsoft.Network/virtualNetworks/subnets -ParentResource virtualNetworks/VNET-EASTUS-01 -ResourceGroupName Network
