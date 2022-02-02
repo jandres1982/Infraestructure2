e1953bbda64214d52413f1e3f71f7484b77e7b8c
Nahum Sancho Puri <nahum.sancho@schindler.com>
2022-02-02 13:36:27 +0000

    Updated volume-C-extend.ps1

--- scripts/disks/volume-C-extend.ps1
+++ scripts/disks/volume-C-extend.ps1
@@ -1,9 +1,7 @@
-# Refresh available space
-"rescan" | diskpart
-
 # Variable specifying the drive you want to extend
 $letter="C"
-
+# Refresh available space
+"rescan" | diskpart
 # Script to get the partition sizes and then resize the volume
 $MaxSize = (Get-PartitionSupportedSize -DriveLetter $letter).sizeMax
 Resize-Partition -DriveLetter $letter -Size $MaxSize
