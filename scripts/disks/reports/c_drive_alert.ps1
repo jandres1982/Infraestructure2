7cd3961fdc3d9d0551efb9336c6a91ff5feea423
Antonio Vento <antoniovicente.vento@schindler.com>
2022-02-03 10:02:08 +0100

    exit code

--- scripts/disks/reports/c_drive_alert.ps1
+++ scripts/disks/reports/c_drive_alert.ps1
@@ -1,9 +1,9 @@
 $free_mb = $(Get-WmiObject Win32_Volume | Where-Object {$_.DriveLetter -eq "c:" }).FreeSpace 
 $free_gb = [math]::round($free_mb/1Gb,0)
-if ($free_gb -lt "200")
+if ($free_gb -lt "20")
 {write-host "Warning C:\ drive near to get full, extend or check asap"
-$Alert = $true
+exit 1
 }else
 {write-host "All Ok"
-$Alert = $false
+exit 0
 }
