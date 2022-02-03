625717e9f5a6a4db21a51d9052307efa0827c92b
Antonio Vento <antoniovicente.vento@schindler.com>
2022-02-03 09:56:33 +0100

    alert true

--- scripts/disks/reports/c_drive_alert.ps1
+++ scripts/disks/reports/c_drive_alert.ps1
@@ -1,6 +1,6 @@
 $free_mb = $(Get-WmiObject Win32_Volume | Where-Object {$_.DriveLetter -eq "c:" }).FreeSpace 
 $free_gb = [math]::round($free_mb/1Gb,0)
-if ($free_gb -lt "20")
+if ($free_gb -lt "200")
 {write-host "Warning C:\ drive near to get full, extend or check asap"
 $Alert = $true
 }else
