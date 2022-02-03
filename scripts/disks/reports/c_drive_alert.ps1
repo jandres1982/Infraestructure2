3d587b15c427a14c8e3000895a02f3c7b29e4edd
Antonio Vento <antoniovicente.vento@schindler.com>
2022-02-03 11:01:31 +0100

    adding email send

--- scripts/disks/reports/c_drive_alert.ps1
+++ scripts/disks/reports/c_drive_alert.ps1
@@ -52,3 +52,20 @@ Write-host "Keep working"
 
 }#end for each sub
 
+$PSEmailServer = "smtp.eu.schindler.com"
+$From = "scc-support-zar.es@schindler.com"
+#$to = "gda_usr_dcff050b-8326-48c9-8bf9-61f8de7e89f0@schindler.com","gdl_usr_7aabcc1e-97e6-4283-9271-c04245556940@cloud.schindler.com"
+$to = "antoniovicente.vento@schindler.com"
+
+$Subject = "C Drive Alert for All Windows Azure Servers"
+#$Filename = Get-ChildItem $Path -Name "Att*" | select -Last 1
+$Attachment = (get-childitem "*.csv")
+$Body = @"
+Dear team,
+
+Please find attached the Report for Backup Jobs for Azure Servers.
+
+
+"@
+
+Send-MailMessage -From $From -To $To -Subject $Subject -Body $Body -Attachments $Attachment
