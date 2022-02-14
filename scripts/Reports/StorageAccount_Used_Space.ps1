e924a1b66ed1040379d58e4f108573f421949716
Nahum Sancho <nahum.sancho@schindler.com>
2022-02-14 11:13:53 +0100

    StorageAccount_Used_Space v1.1

--- scripts/Reports/StorageAccount_Used_Space.ps1
+++ scripts/Reports/StorageAccount_Used_Space.ps1
@@ -1,6 +1,12 @@
-$storges=Get-AzStorageAccount
+$subs=Get-AzSubscription | Where-Object {$_.Name -match "s-sis-*"} 
+foreach ($sub in $subs)
+{
+set-azcontext -Subscription $sub.Name
+$storages=Get-AzStorageAccount
 foreach ($storage in $storages)
 {
-$capacity=Get-AzMetric -ResourceId $storage.id -MetricFilter "UsedCapacity" -AggregationType Average
-$capacity.data
+$capacity=Get-AzMetric -ResourceId $storage.id -MetricName "UsedCapacity" -AggregationType Average
+$storage.StorageAccountName | Export-Csv -Path .\storage_used_space.csv
+$capacity.data | Export-Csv -Path .\storage_used_space.csv
+}
 }
