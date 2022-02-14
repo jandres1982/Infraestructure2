adf68ba1bf4deef9b5fa0136b5eb0474373bdb56
Nahum Sancho <nahum.sancho@schindler.com>
2022-02-14 13:46:05 +0100

    Finaly StorageAccount_Used_Space

--- scripts/Reports/StorageAccount_Used_Space.ps1
+++ scripts/Reports/StorageAccount_Used_Space.ps1
@@ -1,13 +1,15 @@
-$subs=Get-AzSubscription | Where-Object {$_.Name -match "s-sis-*"} 
+$subs=Get-AzSubscription | Where-Object {$_.Name -match "s-sis-*"}
 foreach ($sub in $subs)
 {
 set-azcontext -Subscription $sub.Name
 $storages=Get-AzStorageAccount
 foreach ($storage in $storages)
 {
-$capacity=Get-AzMetric -ResourceId $storage.id -MetricName "UsedCapacity" -AggregationType Average
-$storatagename=$storage.StorageAccountName
-$storagename=$storagename + " " + $capacity.data
-write-host $storagename
+$capacity=Get-AzMetric -ResourceId $storage.id -MetricName "UsedCapacity" -AggregationType Average -WarningAction SilentlyContinue
+$St_name = $storage.StorageAccountName
+[int64]$value = $capacity.Data.average / 1024
+$value = $value /1024
+$Subscription = $sub.name
+Write-output "$Subscription,$St_name,$value" >> Storage_account_Size.txtls
 }
 }
