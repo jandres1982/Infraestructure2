21d16170100a2ad392ddef9b14b5dff1eff8d07e
Antonio Vento <antoniovicente.vento@schindler.com>
2022-02-20 21:23:35 +0100

    check hostname

---
+++ scripts/Server_provisioning/AD/Check_Hostname.ps1
@@ -0,0 +1,8 @@
+$check = Get-ADComputer -Filter 'Name -like $vm'
+if ($check.Name -eq $vm)
+{
+write-host "$vm exist, cannot be used"
+Write-Error "$vm cannot be used"
+}else
+{write-host "$vm can be used"
+}
