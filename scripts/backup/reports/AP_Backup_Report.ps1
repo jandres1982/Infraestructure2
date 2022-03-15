ea486ed4681753b73a482a5c628ba03b47dd6757
Antonio Vento <antoniovicente.vento@schindler.com>
2022-03-15 14:27:07 +0100

    Remove Alfonso

--- scripts/backup/reports/AP_Backup_Report.ps1
+++ scripts/backup/reports/AP_Backup_Report.ps1
@@ -26,7 +26,7 @@ $Jobs | Export-Csv -Path "Backup_Report_$kg_$date.csv" -Append -Force
 
 $PSEmailServer = "smtp.eu.schindler.com"
 $From = "scc-support-zar.es@schindler.com"
-$to = "alfonso.marques@schindler.com","antoniovicente.vento@schindler.com"
+$to = "antoniovicente.vento@schindler.com"
 
 $Subject = "Backup Report $kg Servers"
 #$Filename = Get-ChildItem $Path -Name "Att*" | select -Last 1
