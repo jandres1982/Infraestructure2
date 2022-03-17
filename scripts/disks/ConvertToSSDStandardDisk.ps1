08a0160d4a24017e7a81f4648fe1d1f57be0f477
Antonio Vento <antoniovicente.vento@schindler.com>
2022-03-17 14:06:33 +0100

    push code

--- scripts/disks/ConvertToSSDStandardDisk.ps1
+++ scripts/disks/ConvertToSSDStandardDisk.ps1
@@ -1,14 +1,12 @@
-
 param([string]$vm)
-
 #$subs = @("s-sis-eu-nonprod-01","s-sis-ap-prod-01","s-sis-eu-prod-01","s-sis-am-prod-01","s-sis-am-nonprod-01")
-
 #param([string]$vm)
 #$vm = "shhwsr2242"
-$subs = @("s-sis-am-prod-01","s-sis-am-nonprod-01")
+$subs = @("s-sis-am-nonprod-01","s-sis-am-nonprod-01")
 
 foreach ($sub in $subs)
 {
+    Set-AzContext -Subscription $sub
     Select-AzSubscription -Subscription $sub
     az account set --subscription $sub
     #$vm = get-azvm -Name $vm
@@ -18,8 +16,8 @@ foreach ($sub in $subs)
                 $vm = get-azvm -Name $vm
                 $rg = $vm.ResourceGroupName
                 Write-Host "Working in $rg"
-                az vm stop --resource-group $rg --name $vm
-                az vm deallocate -g $rg -n $vm
+                az vm stop --resource-group $rg --name $vm.name
+                az vm deallocate -g $rg -n $vm.name
                 $osdisk = $vm.StorageProfile.OSDisk.Name
                 az disk update -g $rg -n $osdisk --sku StandardSSD_LRS
                 $datadisk = $vm.StorageProfile.DataDisks.Name
@@ -27,7 +25,7 @@ foreach ($sub in $subs)
                         {
                         az disk update -g $rg -n $disk --sku StandardSSD_LRS
                         }
-                         az vm start -g $rg -n $vm
+                         az vm start -g $rg -n $vm.name
            
             }else
                  {
