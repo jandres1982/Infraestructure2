bf7e03bf9c2ef26bc6b1b794b54c7593493dfca9
Antonio Vento <antoniovicente.vento@schindler.com>
2022-03-17 13:45:08 +0100

    push

--- scripts/disks/ConvertToSSDStandardDisk.ps1
+++ scripts/disks/ConvertToSSDStandardDisk.ps1
@@ -1,18 +1,21 @@
 
-param([string]$vm)
+#param([string]$vm)
 
-$subs = @("s-sis-eu-nonprod-01","s-sis-ap-prod-01","s-sis-eu-prod-01","s-sis-am-prod-01","s-sis-am-nonprod-01")
+#$subs = @("s-sis-eu-nonprod-01","s-sis-ap-prod-01","s-sis-eu-prod-01","s-sis-am-prod-01","s-sis-am-nonprod-01")
+
+#param([string]$vm)
+$vm = "shhwsr2242"
+$subs = @("s-sis-am-prod-01","s-sis-am-nonprod-01")
 
 foreach ($sub in $subs)
 {
     Select-AzSubscription -Subscription "$sub"
-    $vm = get-azvm -Name $vm
-        if ($vm -eq $null)
+    #$vm = get-azvm -Name $vm
+        if (get-azvm -Name $vm)
             {
-            write-host "$vm is not in Azure $sub"
-            }else
-                {
                 az account set --subscription $sub
+                Write-Host "Working in $vm"
+                $vm = get-azvm -Name $vm
                 $rg = $vm.ResourceGroupName
                 az vm stop --resource-group $rg --name $vm
                 az vm deallocate -g $rg -n $vm
@@ -23,7 +26,11 @@ foreach ($sub in $subs)
                         {
                         az disk update -g $rg -n $disk --sku StandardSSD_LRS
                         }
-                az vm start -g $rg -n $vm
+                         az vm start -g $rg -n $vm
+           
+            }else
+                 {
+                 Write-host "$vm is not found in $sub"
 
-                }
+                 }
 }
