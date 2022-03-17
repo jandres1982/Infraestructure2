92302bedf40b22444b310f041b9ede6327debb1b
Antonio Vento <antoniovicente.vento@schindler.com>
2022-03-17 12:42:01 +0100

    ConvertToSSD

--- scripts/disks/ConvertToSSDStandardDisk.ps1
+++ scripts/disks/ConvertToSSDStandardDisk.ps1
@@ -1,29 +1,29 @@
 
+param([string]$vm)
 
+$subs = @("s-sis-eu-nonprod-01","s-sis-ap-prod-01","s-sis-eu-prod-01","s-sis-am-prod-01","s-sis-am-nonprod-01")
 
-
-
-#select sub
-Select-AzSubscription -Subscription $(sub)
-Set-AzContext -subscription $(sub)
-az account set --subscription $(sub)
-
-#main
-az vm stop --resource-group $(rg) --name $(vm)
-az vm deallocate -g $(rg) -n $(vm)
-
-#---Define VM
-$virtualmachine = Get-AzVm -Name $(vm) -ResourceGroupName $(rg)
-
-#--- OSDisk
-$osdisk = $virtualmachine.StorageProfile.OSDisk.Name
-az disk update -g $(rg) -n $osdisk --sku StandardSSD_LRS
-
-#---- DataDisks
-$datadisk = $virtualmachine.StorageProfile.DataDisks.Name
-foreach ($disk in $datadisk)
+foreach ($sub in $subs)
 {
-az disk update -g $(rg) -n $disk --sku StandardSSD_LRS
-}
+    Select-AzSubscription -Subscription "$sub"
+    $vm = get-azvm -Name $vm
+        if ($vm -eq $null)
+            {
+            write-host "$vm is not in Azure $sub"
+            }else
+                {
+                az account set --subscription $(sub)
+                $rg = $vm.resourcegroupname
+                az vm stop --resource-group $rg --name $vm
+                az vm deallocate -g $rg -n $vm
+                $osdisk = $vm.StorageProfile.OSDisk.Name
+                az disk update -g $rg -n $osdisk --sku StandardSSD_LRS
+                $datadisk = $vm.StorageProfile.DataDisks.Name
+                    foreach ($disk in $datadisk)
+                        {
+                        az disk update -g $rg -n $disk --sku StandardSSD_LRS
+                        }
+                az vm start -g $rg -n $vm
 
-az vm start -g $(rg) -n $(vm)
+                }
+}
