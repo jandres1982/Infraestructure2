2b8984d761e531e06e3e3ce22c2f5034d089e985
Antonio Vento <antoniovicente.vento@schindler.com>
2022-03-22 15:31:51 +0100

    confirm changes

--- scripts/license/hybrid_benefit_windows.ps1
+++ scripts/license/hybrid_benefit_windows.ps1
@@ -1,2 +1,36 @@
-$vm = Get-AzVM -ResourceGroup "rg-name" -Name "vm-name"$vm.LicenseType = "Windows_Server"
-Update-AzVM -ResourceGroupName rg-name -VM $vm
+
+#param([string]$vmName)
+#$subs = @("s-sis-eu-nonprod-01","s-sis-ap-prod-01","s-sis-eu-prod-01","s-sis-am-prod-01","s-sis-am-nonprod-01")
+#param([string]$vm)
+#$vm = "shhwsr2242"
+$Servers = Get-Content -Path "Server_List_Hybrid_Benefit.txt"
+
+foreach ($vmName in $Servers)
+{
+    
+#$subs = @("s-sis-eu-nonprod-01","s-sis-ap-prod-01","s-sis-eu-prod-01","s-sis-am-prod-01","s-sis-am-nonprod-01")
+$subs = @("s-sis-eu-nonprod-01")
+foreach ($sub in $subs)
+{
+    Select-AzSubscription -Subscription $sub
+
+# Choose between Standard_LRS, StandardSSD_LRS and Premium_LRS based on your scenario
+
+if (get-azvm -Name $vmName)
+    {
+    Write-Host "Working in $vmName"
+    $vm = get-azvm -Name $vmName
+    $rg = $vm.ResourceGroupName
+    $vm.LicenseType = "Windows_Server"
+    Update-AzVM -ResourceGroupName $rg -vm $vm
+    Write-host "$vmName should have been assigned to Windows Server license type:"
+    $vm.LicenseType
+
+    }else
+    {
+    Write-host "$vmName is not found in $sub"
+    }
+}
+
+
+}
