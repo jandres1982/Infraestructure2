cbb5eef87a8e8212b94f28cbcfd855ba3e3b5f30
Antonio Vento <antoniovicente.vento@schindler.com>
2022-04-01 10:35:20 +0200

    To distribution list

--- scripts/backup/reports/Full/ALL_Backup_Report_Full.ps1
+++ scripts/backup/reports/Full/ALL_Backup_Report_Full.ps1
@@ -75,7 +75,7 @@ $vmBackupReport | Export-Csv $report
 
 $PSEmailServer = "smtp.eu.schindler.com"
 $From = "scc-support-zar.es@schindler.com"
-$to = "antoniovicente.vento@schindler.com","nahum.sancho@schindler.com","alfonso.marques@schindler.com"
+$to = "gda_usr_dcff050b-8326-48c9-8bf9-61f8de7e89f0@schindler.com","gdl_usr_7aabcc1e-97e6-4283-9271-c04245556940@cloud.schindler.com"
 
 $Subject = "Backup Report $kg Servers"
 #$Filename = Get-ChildItem $Path -Name "Att*" | select -Last 1
