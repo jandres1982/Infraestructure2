61b1d85763b50dcbb15bd9a0a4f76088928f47c1
Nahum Sancho Puri <nahum.sancho@schindler.com>
2022-04-06 07:13:09 +0000

    RG mail to eusebio.rodriguez@schindler.com

---
+++ scripts/resourcegroup/send-rg-creation.ps1
@@ -0,0 +1,21 @@
+$PSEmailServer = "smtp.eu.schindler.com"
+$From = "scc-support-zar.es@schindler.com"
+$to = "eusebio.rodriguez@schindler.com"
+
+$Subject = "New Resource Group $(name)"
+$Body = @"
+Dear Eusebio, <br /> <br />
+
+The resource group called $(name) has been created with the following tags:<br /> <br />
+Application Owner: $(applicationowner) <br />
+Cost Center: $(costcenter) <br />
+Infrastructure Service: $(infrastructureservice) <br />
+KG: $(kg) <br />
+Service Owner: $(serviceowner) <br />
+Technical Contact: $(technicalcontact) <br /> <br />
+
+Best regards. <br /> <br />
+
+Azure Team
+"@
+Send-MailMessage -From $From -To $To -Subject $Subject -Body $Body -BodyAsHtml
