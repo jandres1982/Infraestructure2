f7e6646eb6b6428aaaa9c2ef256da9e5659df91b
Nahum Sancho Puri <nahum.sancho@schindler.com>
2022-04-06 07:30:49 +0000

    Updated send-rg-creation.ps1

--- scripts/resourcegroup/send-rg-creation.ps1
+++ scripts/resourcegroup/send-rg-creation.ps1
@@ -1,18 +1,19 @@
+param([string]$name,[string]$applicationowner,[string]$costcenter,[string]$infrastructureservice,[string]$kg,[string]$serviceowner,[string]$technicalcontact)
 $PSEmailServer = "smtp.eu.schindler.com"
 $From = "scc-support-zar.es@schindler.com"
 $to = "eusebio.rodriguez@schindler.com"
 
-$Subject = "New Resource Group $(name)"
+$Subject = "New Resource Group $name"
 $Body = @"
 Dear Eusebio, <br /> <br />
 
-The resource group called $(name) has been created with the following tags:<br /> <br />
-Application Owner: $(applicationowner) <br />
-Cost Center: $(costcenter) <br />
-Infrastructure Service: $(infrastructureservice) <br />
-KG: $(kg) <br />
-Service Owner: $(serviceowner) <br />
-Technical Contact: $(technicalcontact) <br /> <br />
+The resource group called $name has been created with the following tags:<br /> <br />
+Application Owner: $applicationowner <br />
+Cost Center: $costcenter <br />
+Infrastructure Service: $infrastructureservice <br />
+KG: $kg <br />
+Service Owner: $serviceowner <br />
+Technical Contact: $technicalcontact <br /> <br />
 
 Best regards. <br /> <br />
 
