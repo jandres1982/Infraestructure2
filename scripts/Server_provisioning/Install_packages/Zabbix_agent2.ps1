6d5cf358ecfa2b7572a748b080e394901b677bd6
Javier Andres Roy <javier.roy@schindler.com>
2022-04-07 08:47:05 +0000

    Updated Zabbix_agent2.ps1

--- scripts/Server_provisioning/Install_packages/Zabbix_agent2.ps1
+++ scripts/Server_provisioning/Install_packages/Zabbix_agent2.ps1
@@ -1,3 +1,6 @@
 # Unistall Zabbix agent 1
+
 cmd.exe /c "C:\Program Files\Zabbix\bin>zabbix_agentd.exe -c "C:\Program Files\Zabbix\conf\zabbix_agentd.win.conf" -d"
 start-sleep 60
+
+# Install Zabbix agent 2
