0b7a95545f6ed77aa15d35baf49ee67182dd9381
Javier Andres Roy <javier.roy@schindler.com>
2022-04-07 09:14:00 +0000

    Updated Zabbix_agent2.ps1

--- scripts/Server_provisioning/Install_packages/Zabbix_agent2.ps1
+++ scripts/Server_provisioning/Install_packages/Zabbix_agent2.ps1
@@ -1,12 +1,12 @@
 # Unistall Zabbix agent 1
 
-cmd.exe /c "C:\Program Files\Zabbix\bin>zabbix_agentd.exe -c "C:\Program Files\Zabbix\conf\zabbix_agentd.win.conf" -d"
-start-sleep 60
+#cmd.exe /c "C:\Program Files\Zabbix\bin>zabbix_agentd.exe -c "C:\Program Files\Zabbix\conf\zabbix_agentd.win.conf" -d"
+#start-sleep 60
 
 # Install Zabbix agent 2
 
-cmd.exe /c "C:\provision\Schindler\Zabbix_agent2\zabbix_agent2.exe --install"
+cmd.exe /c "C:\tmp\Zabbix_agent2\bin\zabbix_agent2.exe --install"
 
 # Start Zabbix agent 2
 
-cmd.exe /c "C:\provision\Schindler\Zabbix_agent2\zabbix_agent2.exe --start"
+cmd.exe /c "C:\tmp\Zabbix_agent2\zabbix_agent2.exe --start"
