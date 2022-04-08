4f513a3235ecc6ba95b0b2f9b7439706893abe68
Antonio Vicente Vento Maggio <antoniovicente.vento@schindler.com>
2022-04-08 09:43:16 +0000

    Updated Zabbix_agent2.ps1

--- scripts/Server_provisioning/Install_packages/Zabbix_agent2.ps1
+++ scripts/Server_provisioning/Install_packages/Zabbix_agent2.ps1
@@ -1,7 +1,13 @@
 # Unistall Zabbix agent 1
-
 #cmd.exe /c "C:\Program Files\Zabbix\bin>zabbix_agentd.exe -c "C:\Program Files\Zabbix\conf\zabbix_agentd.win.conf" -d"
+
 #start-sleep 60
+
+# Copy Zabbix folder from provision temporary folder to the server
+
+Copy-Item -Path C:\provision\Zabbix_6.0_Agent_v2 -Destination "C:\Program Files" -recurse -force
+
+start-sleep 20
 
 # Set Hostname
 
@@ -12,6 +18,7 @@ $vm = hostname
 # Install Zabbix agent 2
 
 cmd.exe /c "C:\tmp\Zabbix_agent2\bin\zabbix_agent2.exe --install"
+
 start-sleep 30
 
 
