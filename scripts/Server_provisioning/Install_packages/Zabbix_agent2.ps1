9d4a5207d67c8f517f19f0af7e35cba944df330f
Javier Andres Roy <javier.roy@schindler.com>
2022-04-08 07:46:49 +0000

    Updated Zabbix_agent2.ps1

--- scripts/Server_provisioning/Install_packages/Zabbix_agent2.ps1
+++ scripts/Server_provisioning/Install_packages/Zabbix_agent2.ps1
@@ -3,14 +3,17 @@
 #cmd.exe /c "C:\Program Files\Zabbix\bin>zabbix_agentd.exe -c "C:\Program Files\Zabbix\conf\zabbix_agentd.win.conf" -d"
 #start-sleep 60
 
+# Set Hostname
+
+$vm = hostname
+
+(Get-Content -Path 'C:\tmp\Zabbix_agent2\bin\zabbix_agent2.win.conf') -replace 'vm',$vm | Set-Content -Path C:\tmp\Zabbix_agent2\bin\zabbix_agent2.win.conf
+
 # Install Zabbix agent 2
 
 cmd.exe /c "C:\tmp\Zabbix_agent2\bin\zabbix_agent2.exe --install"
 start-sleep 30
 
-# Set Hostname
-
-(Get-Content -Path 'C:\tmp\Zabbix_agent2\bin\zabbix_agent2.win.conf') -replace 'vm',$vm | Set-Content -Path C:\tmp\Zabbix_agent2\bin\zabbix_agent2.win.conf
 
 # Start Zabbix agent 2
 
