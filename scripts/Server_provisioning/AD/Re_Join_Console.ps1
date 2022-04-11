2cd40c2fa29899c24045d84aa681fe925c0cd806
Antonio <azdevopsantonio1707@outlook.com>
2022-04-11 18:13:23 +0200

    re join

--- scripts/Server_provisioning/AD/Re_Join_Console.ps1
+++ scripts/Server_provisioning/AD/Re_Join_Console.ps1
@@ -8,7 +8,7 @@ powershell.exe
 
 #Powershell rejoin
 $PW = ""
-$password = $PW | ConvertTo-SecureString -as
+$password = $PW | ConvertTo-SecureString -AsPlainText -Force
 $domain = "global.schindler.com"
 $cred = New-Object System.Management.Automation.PSCredential -ArgumentList ("",$password)
 test-computersecurechannel -repair -credential $cred
