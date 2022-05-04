62d1ed38951d94c002824280c44e7c9678f411c4
Javier Andres Roy <javier.roy@schindler.com>
2022-05-04 13:05:44 +0000

    Updated Zabbix_agent2.ps1

--- scripts/Server_provisioning/Install_packages/Zabbix_agent2.ps1
+++ scripts/Server_provisioning/Install_packages/Zabbix_agent2.ps1
@@ -5,7 +5,7 @@
 
 # Copy Zabbix folder from provision temporary folder to the server
 
-Copy-Item -Path "C:\provision\Schindler\Zabbix_6.0_Agent_v2" -Destination "C:\Program Files" -recurse -force
+#Copy-Item -Path "C:\provision\Schindler\Zabbix_6.0_Agent_v2" -Destination "C:\Program Files" -recurse -force
 
 #start-sleep 20
 
