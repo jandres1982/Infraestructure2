a97982a344dd773f236ec72f910ed6afd9248ac3
Antonio Vento <antoniovicente.vento@schindler.com>
2022-05-18 09:19:56 +0200

    AntiVirus Defender

--- scripts/Server_provisioning/Install_packages/AntiVirus.ps1
+++ scripts/Server_provisioning/Install_packages/AntiVirus.ps1
@@ -8,13 +8,13 @@ cmd.exe /c "c:\provision\Microsoft\Defender\2019\WindowsDefenderATPOnboardingScr
 $OSVersion = (Get-WMIObject win32_operatingsystem).caption
 If ($OSVersion -like '*2016*')
 {
-cmd.exe /c "c:\Windows\System32\msiexec.exe /i /quiet c:\provision\Microsoft\Defender\2012-2016\WindowsServer_2012_2016\md4ws.msi"
+cmd.exe /c "c:\Windows\System32\msiexec.exe /i c:\provision\Microsoft\Defender\2012-2016\WindowsServer_2012_2016\md4ws.msi /quiet"
 cmd.exe /c "c:\provision\Microsoft\Defender\2012-2016\WindowsServer_2012_2016\WindowsDefenderATPOnboardingScript.cmd"
 }
 
 $OSVersion = (Get-WMIObject win32_operatingsystem).caption
 If ($OSVersion -like '*2012*')
 {
-cmd.exe /c "c:\Windows\System32\msiexec.exe /i /quiet c:\provision\Microsoft\Defender\2012-2016\WindowsServer_2012_2016\md4ws.msi"
+cmd.exe /c "c:\Windows\System32\msiexec.exe /i c:\provision\Microsoft\Defender\2012-2016\WindowsServer_2012_2016\md4ws.msi /quiet"
 cmd.exe /c "c:\provision\Microsoft\Defender\2012-2016\WindowsServer_2012_2016\WindowsDefenderATPOnboardingScript.cmd"
 }
