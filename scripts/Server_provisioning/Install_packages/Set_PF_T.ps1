cc812480e97e081de6c7e3d1a99227414f7f8135
Antonio Vento <antoniovicente.vento@schindler.com>
2022-06-03 13:37:36 +0200

    Pagefile to T

--- scripts/Server_provisioning/Install_packages/Set_PF_T.ps1
+++ scripts/Server_provisioning/Install_packages/Set_PF_T.ps1
@@ -1,5 +1,3 @@
-Set-Itemproperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management' -Name 'PagingFiles' -value 'C:\pagefile.sys 0 0'
-restart-computer -force
 $Temp_Disk = Get-volume | Where-Object -Property FileSystemLabel -eq -Value "Temporary Storage"
 if ($Temp_Disk)
 {
