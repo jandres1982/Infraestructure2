dda57d050e5491e9c9d317fa8f203c0addd7dd0b
Nahum Sancho <nahum.sancho@schindler.com>
2022-06-08 16:45:27 +0200

    Last azure moduules for docker

--- Projects/Docker-Compose/Devops_Agents/install_modules.ps1
+++ Projects/Docker-Compose/Devops_Agents/install_modules.ps1
@@ -1,4 +1,8 @@
 $ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows  -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
-install-module -name az -Force
-install-module -name Azure.Service -Force
-install-module -name Azure.Compute -Force
+Install-PackageProvider -Name NuGet -Force
+Get-PackageProvider
+Get-PackageSource
+Install-Module -Name Az.Compute -Repository PSGallery -Force
+Install-Module -Name Az.Accounts -Repository PSGallery -Force
+Install-Module -Name Az.Service -Repository PSGallery -Force
+Get-Module
