31b958a9e2224cf8b810b9ff5e9ad029b0415c8f
Nahum Sancho Puri <nahum.sancho@schindler.com>
2022-06-10 07:42:33 +0000

    Updated install_modules.ps1

--- Projects/Docker-Compose/Devops_Agents/install_modules.ps1
+++ Projects/Docker-Compose/Devops_Agents/install_modules.ps1
@@ -1,8 +1,1 @@
 $ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows  -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
-Install-PackageProvider -Name NuGet -Force
-Get-PackageProvider
-Get-PackageSource
-Install-Module -Name Az.Compute -Repository PSGallery -Force
-Install-Module -Name Az.Accounts -Repository PSGallery -Force
-Install-Module -Name Az.Service -Repository PSGallery -Force
-Get-Module
