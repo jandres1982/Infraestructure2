1dcb2225f13fd36b7342623715a11a20d4e283c7
Antonio Vento <antoniovicente.vento@schindler.com>
2022-06-15 10:38:48 +0200

    DomainJoin

--- scripts/Server_provisioning/AD/AD_KeyVault/DomainJoinKeyVault.ps1
+++ scripts/Server_provisioning/AD/AD_KeyVault/DomainJoinKeyVault.ps1
@@ -2,6 +2,5 @@ param([string]$joinuser,[string]$joinpw)
 $password = $joinpw| ConvertTo-SecureString -AsPlainText -Force
 $domain = "global.schindler.com"
 $cred = New-Object System.Management.Automation.PSCredential -ArgumentList ($joinuser,$password)
-write-output "$joinuser"
-write-output "$joinpw"
 Add-Computer -DomainName $domain -Credential $cred
+Restart-Computer -Force
