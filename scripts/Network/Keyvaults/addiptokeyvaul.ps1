570f81b37a4de8dd52ea91e2eda2e2d65f6a96fd
Alberto Delgado Alvarez <alberto.delgado@schindler.com>
2022-07-20 09:29:59 +0000

    Updated addiptokeyvaul.ps1

--- scripts/Network/Keyvaults/addiptokeyvaul.ps1
+++ scripts/Network/Keyvaults/addiptokeyvaul.ps1
@@ -2,6 +2,10 @@
 ##    Add IP ranges to Key vaults. Please, full fill ipranges.txt
 ## --------------------------------------------------------------*/
 
+#$subsriptionId = “subscription guid”
+#Select-AzSubscription -Subscription $subsriptionId
+#$ipranges = ((Get-AzNetworkServiceTag -Location eastus).Values | Where-Object { $_.Name -iin ("PowerPlatformInfra.NorthEurope" ,"PowerPlatformInfra.WestEurope")}).properties.AddressPrefixes
+
 $myvault = "kv-prod-ssot-01"
 $ipranges = gc "ipranges.txt"
 foreach ($iprange in $ipranges)
