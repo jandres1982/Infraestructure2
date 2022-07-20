6a67e13c31b461cd3b4e7c84cf85002fdddc9d0e
Antonio Vicente Vento Maggio <antoniovicente.vento@schindler.com>
2022-07-20 09:24:26 +0000

    Updated addiptostorageaccount.ps1

--- scripts/Network/Datalakes/addiptostorageaccount.ps1
+++ scripts/Network/Datalakes/addiptostorageaccount.ps1
@@ -2,6 +2,10 @@
 ##   Add ip ranges to STORAGE Account please fill ipranges.txt, before running the script, thanks 
 ## ----------------------------------------------------------------*/
 
+#$subsriptionId = “subscription guid”
+#Select-AzSubscription -Subscription $subsriptionId
+#$ipranges = ((Get-AzNetworkServiceTag -Location eastus).Values | Where-Object { $_.Name -iin ("PowerPlatformInfra.NorthEurope" ,"PowerPlatformInfra.WestEurope")}).properties.AddressPrefixes
+
 $myvault = "dlsprodssot04"
 $rg = "rg-cis-nonprod-ssot-01"
 $ipranges = gc "ipranges.txt"
