ccbde7a066855266e2975347b5e276e9fb8d4a41
Nahum Sancho <nahum.sancho@schindler.com>
2022-07-26 13:08:25 +0200

    Add IPS to Storage Account

---
+++ scripts/Storage/add_ips_storage.ps1
@@ -0,0 +1,11 @@
+
+###Set subscription
+set-azcontext -subscription $(sub)
+###Set the keyvault
+$st=Get-AzStorageAccount | where {$_.StorageAccountName -eq "$(storageaccount)"}
+
+$ips= Get-Content "publicip.txt"
+foreach ($ip in $ips)
+{
+    Add-AzStorageAccountNetworkRule -ResourcegroupName $st.ResourcegroupName -Name $st.StorageAccountName -IpAddressRange "$ip" 
+}
