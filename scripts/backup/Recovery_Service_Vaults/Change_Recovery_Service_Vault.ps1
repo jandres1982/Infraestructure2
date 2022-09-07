505834139f6f61bb8ff15d79d9dbeb011bd52408
Antonio Vento <antoniovicente.vento@schindler.com>
2022-09-07 08:11:02 +0200

    change completed

--- scripts/backup/Recovery_Service_Vaults/Change_Recovery_Service_Vault.ps1
+++ scripts/backup/Recovery_Service_Vaults/Change_Recovery_Service_Vault.ps1
@@ -3,6 +3,8 @@ $sub = "s-sis-eu-nonprod-01"
 $vm = "zzzwsr0010"
 $RSV = "rsv-nonprod-euno-lrsbackup-02"
 $RSV_RG = "rg-cis-nonprod-backup-01"
+$location = "northeurope"
+$Virtual_Machine =  get-azvm -name $vm
 
 ##
 Select-AzSubscription -Subscription $SUB
@@ -15,8 +17,7 @@ $PI = Get-AzRecoveryServicesBackupItem -Container $Cont[0] -WorkloadType AzureVM
 Disable-AzRecoveryServicesBackupProtection -Item $PI[0] -force
 
 #Create_RG
-$location = "northeurope"
-$Virtual_Machine =  get-azvm -name $vm
+
 $vm_rg = $Virtual_Machine.ResourceGroupName
 $Old_rg = get-azresourceGroup -name $VM_RG
 $tags = $Old_rg.tags
@@ -27,17 +28,38 @@ $New_rg = $($old_rg.ResourceGroupName.Remove($old_rg.ResourceGroupName.Length-2)
 New-AzResourceGroup -Name $New_rg -Tag $tags $location
 
 #Move_to_a_new_rg
-$Sub = Get-AzSubscription | Where-Object {$_.Name -match "s-sis-eu-nonprod-01"}
-$nic_id = $($virtual_Machine).NetworkProfile.NetworkInterfaces.id
-Move-AzResource -DestinationResourceGroupName $(Get-AzResourceGroup -Name $New_rg).ResourceGroupName -DestinationSubscriptionId $sub.Id -ResourceId $Virtual_Machine.Id,$nic_id,
-$Data_disk = $($Virtual_Machine.StorageProfile.DataDisks).Name
-Foreach ($disk in $Data_disk)
-{
-    if ($disk -ne $null)
-    {
-    $data_disk_id = $(Get-AzResource -Name $disk).ResourceId
-    Move-AzResource -DestinationResourceGroupName $(Get-AzResourceGroup -Name $New_rg).ResourceGroupName -DestinationSubscriptionId $sub.Id -ResourceId $data_disk_id -force
-    }else
-    {Write-Output "Disk name empty"}
-}
 
+#Get-AzResource -ResourceGroupName $Virtual_Machine.ResourceGroupName | Format-table -wrap -Property ResourceId
+
+$Sub = Get-AzSubscription | Where-Object {$_.Name -match "s-sis-eu-nonprod-01"}
+
+$Virtual_Machine = Get-AzResource -ResourceName $vm
+#moving the VM
+Move-AzResource -ResourceId $Virtual_Machine.ResourceId -DestinationResourceGroupName $New_rg -force
+#moving OS disk
+$Os_disk_id = $(Get-Azvm -Name $Virtual_Machine.name | select-object -Property *).StorageProfile.OsDisk.ManagedDisk.id
+Move-AzResource -ResourceId $Os_disk_id -DestinationResourceGroupName $New_rg -force
+#moving nic
+$nic_id = $(Get-Azvm -Name $Virtual_Machine.name | select-object -Property *).NetworkProfile.NetworkInterfaces.id
+Move-AzResource -ResourceId $nic_id -DestinationResourceGroupName $New_rg -force
+#moving Data Disk
+$Data_disk = $(Get-Azvm -Name $Virtual_Machine.name | select-object -Property *).StorageProfile.DataDisks.Name
+$i = 0
+Foreach ($disk in $Data_disk)
+    {
+    if ($disk -ne $null)
+        {
+        $data_disk_id = $(Get-AzResource -Name $disk).ResourceId
+        Move-AzResource -ResourceId $data_disk_id -DestinationResourceGroupName $New_rg -force   
+        #Write-Output $data_disk_id
+        #New-Variable "DataDisk$i" "$data_disk_id"
+        #$i++
+        #Write-host $i
+    }else
+        {Write-Output "Disk name empty"}
+    }
+
+#Get-AzResource -ResourceGroupName "RG-CIS-TEST-SERVER-01" | Format-table -wrap -Property ResourceId
+
+#$Resource = Get-AzResource -ResourceType "Microsoft.ClassicCompute/virtualmachine" -ResourceName $Virtual_Machine
+#Move-AzResource -ResourceId $Resource.ResourceId -DestinationResourceGroupName "ResourceGroup14"
--- scripts/backup/Snapshots/Runbook_notworking.ps1
+++ scripts/backup/Snapshots/Runbook_notworking.ps1
@@ -8,16 +8,21 @@ param (
 [Parameter(Mandatory = $false)]
 [datetime]$Schedule_Time
 )
+
+$vm= "shhwsr1848"
+$sub = "s-sis-eu-prod-01"
+
 #Name ---> "Snapshots_Schedule"
 Write-Output "$vm"
 Write-Output "$Sub"
 Write-Output "$Retention"
 Write-Output "$Schedule_Time"
 
-$Conn = Get-AutomationConnection -Name AzureRunAsConnection
-Connect-AzAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
+#$Conn = Get-AutomationConnection -Name AzureRunAsConnection
+#Connect-AzAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
 
 ##################  Create a runbook with this data ##############################
+
 
 select-azsubscription -subscription $sub
 set-azcontext -subscription $sub
@@ -35,7 +40,47 @@ Set-AzRecoveryServicesVaultContext -Vault $vault
 
 $backupcontainer = Get-AzRecoveryServicesBackupContainer -ContainerType "AzureVM"  -FriendlyName $Virtual_Machine.name
 $item = Get-AzRecoveryServicesBackupItem -Container $backupcontainer -WorkloadType "AzureVM"
-Backup-AzRecoveryServicesBackupItem -Item $item -ExpiryDateTimeUTC $Schedule_Time.AddDays($Retention)
+$Backup = Backup-AzRecoveryServicesBackupItem -Item $item
+# -ExpiryDateTimeUTC $Schedule_Time.AddDays($Retention)
+
+$Job = Get-AzRecoveryServicesBackupJob -VaultId $vaultid -Jobid $Backup.JobId
+
+while ($job.Status -eq "InProgress")
+{
+
+    Write-Output "Backup is in progress"
+    Start-Sleep 1000
+    $Job = Get-AzRecoveryServicesBackupJob -VaultId $vaultid -Jobid $Backup.JobId
+
+}
+
+
+
+
+$resourceGroup = "resourceGroup"
+$vaultName = "vaultName"
+
+$jobs = az backup job list --resource-group $resourceGroup --vault-name $vaultName --start-date 28-8-2020 | convertfrom-json
+$jobName = $jobs[0].name
+
+$jobStatus = az backup job show --name $jobName --resource-group $resourceGroup --vault-name $vaultName
+$taskStatus = $jobStatus.properties.extendedInfo.tasksList | Where-Object { $_.taskId -eq "Take Snapshot"}
+While ( $taskStatus.status -ne Completed ) {
+     Write-Host -Object "Waiting for completion..."
+     Start-Sleep -minutes 1
+     $jobStatus = az backup job show --name $jobName --resource-group $resourceGroup --vault-name $vaultName
+     $taskStatus = $jobStatus.properties.extendedInfo.tasksList | Where-Object { $_.taskId -eq "Take Snapshot"}
+}
+Write-Host -Object "Done!"
+
+
+
+
+
+
+
+
+
 
 
 
