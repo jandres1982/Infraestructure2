37d90a10bad8008b17a2674cc62d83a44532afb8
Nahum Sancho Puri <nahum.sancho@schindler.com>
2022-09-08 10:45:50 +0000

    Updated snap.tf

--- Terraform/vsphere/snapshot/snap.tf
+++ Terraform/vsphere/snapshot/snap.tf
@@ -24,7 +24,7 @@ data "vsphere_virtual_machine" "proxyveeam" {
 }
 
 resource "vsphere_virtual_machine_snapshot" "proxyveeam" {
-    vsphere_virtual_machine_uuid = data.vsphere_virtual_machine.proxyveeam.id
+    vsphere_virtual_machine_uuid = "${data.vsphere_virtual_machine.proxyveeam.id}"
     snapshot_name = "Snapshot Name"
     desctiption = "This is Demo Snapshot"
     memory = "false"
