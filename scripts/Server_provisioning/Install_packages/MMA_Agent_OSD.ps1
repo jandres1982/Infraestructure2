70c475f19d138039f7092ef1996bb74c6b631370
Nahum Sancho <nahum.sancho@schindler.com>
2022-09-14 15:54:33 +0200

    update script with mma template

--- scripts/Server_provisioning/Install_packages/MMA_Agent_OSD.ps1
+++ scripts/Server_provisioning/Install_packages/MMA_Agent_OSD.ps1
@@ -2,7 +2,7 @@ New-Item -Path ".\MMA_Json" -ItemType "Directory" -Force
 $vm = $args[0]
 $rg = $args[1]
 $Parameters_Base = ".\parameters-eu.json"
-$Template = ".\template.json"
+$Template = ".\mma.json"
 $json = Get-Content $Parameters_Base -raw | convertfrom-json
 $json.parameters.vmName[0].value = "$vm"
 $json | ConvertTo-Json -Depth 32 | Out-File -encoding "UTF8" -FilePath ".\MMA_Json\$vm.json"
