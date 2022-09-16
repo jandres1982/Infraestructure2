4563b6b11f0e0e7ad0ba76f90d3b8b773e262777
Nahum Sancho Puri <nahum.sancho@schindler.com>
2022-09-16 09:32:08 +0000

    Updated Snapshots_Schedule_v1.ps1 with variables using devops release

--- scripts/backup/Snapshots/Snapshots_Schedule_v1.ps1
+++ scripts/backup/Snapshots/Snapshots_Schedule_v1.ps1
@@ -1,32 +1,32 @@
 param (
 [Parameter(Mandatory = $false)]
-[string]$vmName,
+[string]$(vmName),
 [Parameter(Mandatory = $false)]
-[string]$sub,
+[string]$(sub),
 [Parameter(Mandatory = $false)]
-[string]$location,
+[string]$(location),
 [Parameter(Mandatory = $false)]
-[string]$resourceGroup
+[string]$(resourceGroup)
 )
 
 $automationAccountName = "aa-prod-monitoring-01"
-$ResourceGroupName = "rg-cis-prod-monitoring-01"
+$resourceGroupName = "rg-cis-prod-monitoring-01"
 $runbookName = "Disk_Snapshots"
 
 ######################### From Devops ########################
-$vmName = "tstshhwsr0343"
-$sub = "s-sis-eu-nonprod-01"
-$date = "15/09/2022 17:06"
-$ResourceGroup = "rg-shh-test-sharepoint-01"
-$location = "North Europe"
+#$(vmName) = "tstshhwsr0343"
+#$(sub) = "s-sis-eu-nonprod-01"
+#$(date) = "15/09/2022 17:06"
+#$(resourceGroup) = "rg-shh-test-sharepoint-01"
+#$(location) = "North Europe"
 ##############################################################
 
-$Schedule_Time = $date -as [datetime]
+$Schedule_Time = $(date) -as [datetime]
 
-$Snapshot_name = $vmName+"_Snapshot"
-New-AzAutomationSchedule -AutomationAccountName $automationAccountName -Name $Snapshot_name -StartTime $Schedule_Time -ResourceGroupName $ResourceGroupName -OneTime
+$Snapshot_name = $(vmName)+"_Snapshot"
+New-AzAutomationSchedule -AutomationAccountName $automationAccountName -Name $Snapshot_name -StartTime $Schedule_Time -ResourceGroupName $resourceGroupName -OneTime
 
-$Schedule = Get-AzAutomationSchedule -ResourceGroupName $ResourceGroupName -AutomationAccountName $automationAccountName -Name $Snapshot_name
+$Schedule = Get-AzAutomationSchedule -ResourceGroupName $resourceGroupName -AutomationAccountName $automationAccountName -Name $Snapshot_name
 
-$data = @{"vmName"="$vmName";"sub"="$sub";"location"="$location";"resourceGroup"="$resourceGroup"}
-Register-AzAutomationScheduledRunbook –AutomationAccountName $AutomationAccountName –RunbookName $runbookName –ScheduleName $Schedule.Name –Parameters $data -ResourceGroupName $ResourceGroupName
+$data = @{"vmName"="$(vmName)";"sub"="$(sub)";"location"="$(location)";"resourceGroup"="$(resourceGroup)"}
+Register-AzAutomationScheduledRunbook –AutomationAccountName $AutomationAccountName –RunbookName $runbookName –ScheduleName $Schedule.Name –Parameters $data -ResourceGroupName $resourceGroupName
