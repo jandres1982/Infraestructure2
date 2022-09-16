7779642526d134c7994e7065d136f82cb6e5bf50
Nahum Sancho Puri <nahum.sancho@schindler.com>
2022-09-16 10:42:43 +0000

    Updated Snapshots_Schedule_v1.ps1

--- scripts/backup/Snapshots/Snapshots_Schedule_v1.ps1
+++ scripts/backup/Snapshots/Snapshots_Schedule_v1.ps1
@@ -1,12 +1,12 @@
 param (
 [Parameter(Mandatory = $false)]
-[string]$(vmName),
+[string]$vmName,
 [Parameter(Mandatory = $false)]
-[string]$(sub),
+[string]$sub,
 [Parameter(Mandatory = $false)]
-[string]$(location),
+[string]$location,
 [Parameter(Mandatory = $false)]
-[string]$(resourceGroup)
+[string]$resourceGroup
 )
 
 $automationAccountName = "aa-prod-monitoring-01"
@@ -14,19 +14,19 @@ $resourceGroupName = "rg-cis-prod-monitoring-01"
 $runbookName = "Disk_Snapshots"
 
 ######################### From Devops ########################
-#$(vmName) = "tstshhwsr0343"
-#$(sub) = "s-sis-eu-nonprod-01"
+#$vmName = "tstshhwsr0343"
+#$sub = "s-sis-eu-nonprod-01"
 #$(date) = "15/09/2022 17:06"
-#$(resourceGroup) = "rg-shh-test-sharepoint-01"
-#$(location) = "North Europe"
+#$resourceGroup = "rg-shh-test-sharepoint-01"
+#$location = "North Europe"
 ##############################################################
 
 $Schedule_Time = $(date) -as [datetime]
 
-$Snapshot_name = $(vmName)+"_Snapshot"
+$Snapshot_name = $vmName+"_Snapshot"
 New-AzAutomationSchedule -AutomationAccountName $automationAccountName -Name $Snapshot_name -StartTime $Schedule_Time -ResourceGroupName $resourceGroupName -OneTime
 
 $Schedule = Get-AzAutomationSchedule -ResourceGroupName $resourceGroupName -AutomationAccountName $automationAccountName -Name $Snapshot_name
 
-$data = @{"vmName"="$(vmName)";"sub"="$(sub)";"location"="$(location)";"resourceGroup"="$(resourceGroup)"}
+$data = @{"vmName"="$vmName";"sub"="$sub";"location"="$location";"resourceGroup"="$resourceGroup"}
 Register-AzAutomationScheduledRunbook –AutomationAccountName $AutomationAccountName –RunbookName $runbookName –ScheduleName $Schedule.Name –Parameters $data -ResourceGroupName $resourceGroupName
