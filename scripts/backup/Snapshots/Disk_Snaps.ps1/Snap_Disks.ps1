985213256f056b98c8deb97eeea65f13a041d09f
Nahum Sancho Puri <nahum.sancho@schindler.com>
2022-09-22 13:24:23 +0000

    Added Snap_Disks.ps1

---
+++ scripts/backup/Snapshots/Disk_Snaps.ps1/Snap_Disks.ps1
@@ -0,0 +1,29 @@
+select-azsubscription -subscription $(sub)
+set-azcontext -subscription $(sub)
+
+$snapshotName = "Snapshot_$(vm)"
+#$date = get-date
+#Get-Date -Format "dddd MM/dd/yyyy HH:mm K"
+
+$resourceGroup = get-azresourcegroup | Where-Object {$_.ResourceGroupName -match "rg-cis-*-snapshot*"}
+$vmDetails = Get-AzVM | Where-Object {$_.Name -eq "$(vm)"}
+
+#OS DISK SNAPSHOT
+$snapshot =  New-AzSnapshotConfig -SourceUri $vmDetails.StorageProfile.OsDisk.ManagedDisk.Id -Location $location -CreateOption copy
+New-AzSnapshot -Snapshot $snapshot -SnapshotName $snapshotName -ResourceGroupName $resourceGroup
+
+#DATA DISK SNAPSHOT
+$Data_disk = $($vmDetails | select-object -Property *).StorageProfile.DataDisks.Name
+$i = 0
+Foreach ($disk in $Data_disk)
+    {
+    if ($disk -ne $null)
+        {
+        $data_disk_id = $(Get-AzResource -Name $disk).ResourceId
+        $snapshot =  New-AzSnapshotConfig -SourceUri $data_disk_id -Location $location -CreateOption copy
+        $Snap_name = $snapshotName+$i
+        New-AzSnapshot -Snapshot $snapshot -SnapshotName $Snap_name -ResourceGroupName $resourceGroup
+        $i++
+    }else
+        {Write-Output "Disk name empty"}
+    }
