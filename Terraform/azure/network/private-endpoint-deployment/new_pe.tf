582818b09324e88a8e51fde7b63e600a53aa3f64
Nahum Sancho <nahum.sancho@schindler.com>
2022-09-29 10:39:24 +0200

    Test pe with terraform v1.3

--- Terraform/azure/network/private-endpoint-deployment/new_pe.tf
+++ Terraform/azure/network/private-endpoint-deployment/new_pe.tf
@@ -16,6 +16,10 @@ data "azurerm_resource_group" "network-rg" {
   name = "${var.rg-network}"
 }
 
+data "azurerm_resource_group" "pe-rg"{
+  name = "${var.pe-rg}"
+}
+
 data "azurerm_virtual_network" "vnet" {
   name                = "${var.network}"
   resource_group_name = data.azurerm_resource_group.network-rg.name
@@ -25,10 +29,6 @@ data "azurerm_subnet" "subnet" {
   name                 = "${var.subnet}"
   virtual_network_name = data.azurerm_virtual_network.vnet.name
   resource_group_name  = data.azurerm_resource_group.network-rg.name
-}
-
-data "azurerm_resource_group" "pe-rg"{
-  name = "${var.rg-pe}"
 }
 
 #resource "azurerm_private_endpoint" "pe" {
