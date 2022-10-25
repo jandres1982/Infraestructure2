46424303a64461040bffaf5775f26d350cb957d2
Antonio Vicente Vento Maggio <antoniovicente.vento@schindler.com>
2022-10-25 14:43:09 +0000

    Updated Snapshots_Schedule_v1.ps1

--- scripts/backup/Snapshots/Snapshots_Schedule_v1.ps1
+++ scripts/backup/Snapshots/Snapshots_Schedule_v1.ps1
@@ -37,4 +37,4 @@ New-AzAutomationSchedule -AutomationAccountName $automationAccountName -Name $Sn
 $Schedule = Get-AzAutomationSchedule -ResourceGroupName $resourceGroupName -AutomationAccountName $automationAccountName -Name $Snapshot_name
 
 $data = @{"vmName"="$vmName";"sub"="$sub";"location"="$location";"resourceGroup"="$resourceGroup";"date"="$date"}
-Register-AzAutomationScheduledRunbook –AutomationAccountName $AutomationAccountName –RunbookName $runbookName –ScheduleName $Schedule.Name –Parameters $data -ResourceGroupName $resourceGroupName
+Register-AzAutomationScheduledRunbook –AutomationAccountName $AutomationAccountName –Name $runbookName –ScheduleName $Schedule.Name –Parameters $data -ResourceGroupName $resourceGroupName
