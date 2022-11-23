239f45bf0161ab46dbc5fa8d131a4ec6e715693f
Antonio <antoniovicente.vento@schindler.com>
2022-11-23 09:58:42 +0100

    Backup ALL

--- scripts/backup/reports/Full/ALL_Backup_Report_Full.ps1
+++ scripts/backup/reports/Full/ALL_Backup_Report_Full.ps1
@@ -1,5 +1,5 @@
-$subs=Get-AzSubscription | Where-Object {$_.Name -match "s-sis-[aec][upmh]*"}
-#$subs= "s-sis-eu-nonprod-01"
+#$subs=Get-AzSubscription | Where-Object {$_.Name -match "s-sis-[aec][upmh]*"}
+$subs = @("s-sis-eu-nonprod-01","s-sis-eu-prod-01","s-sis-am-prod-01","s-sis-am-nonprod-01","s-sis-ap-prod-01","s-sis-ch-prod-01","s-sis-ch-nonprod-01")
 $date = $(get-date -format yyyy-MM-ddTHH-mm)
 $kg = "ALL"
 ###################################################################
