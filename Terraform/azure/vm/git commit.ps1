b5b027c0272ef8ac2aa1b2375fea6a2704a7addf
Antonio <antoniovicente.vento@schindler.com>
2023-02-16 12:41:07 +0100

    remove file

--- Terraform/azure/vm/git commit.ps1
+++
@@ -1,5 +0,0 @@
-git checkout ventoa1
-cd "C:\Users\ventoa1\OneDrive - Schindler\Azure_Devops\Infraestructure\Terraform\azure\vm"
-git add variables_vm_testv2.tf
-git commit -m test_commit_ventoa1
-git push
