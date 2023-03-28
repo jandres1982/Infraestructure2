4b530fa9e9d54d12017f47096d8d2403249adc5c
Antonio <antoniovicente.vento@schindler.com>
2023-03-28 11:26:16 +0200

    script to change hybrid benefits

--- scripts/license/hybrid_benefit_windows.ps1
+++ scripts/license/hybrid_benefit_windows.ps1
@@ -1,5 +1,4 @@
-
-#param([string]$vmName)
+param($subs)
 #$subs = @("s-sis-eu-nonprod-01","s-sis-ap-prod-01","s-sis-eu-prod-01","s-sis-am-prod-01","s-sis-am-nonprod-01")
 #param([string]$vm)
 #$vm = "shhwsr2242"
@@ -8,7 +7,7 @@ $Servers = Get-Content -Path "Server_List_Hybrid_Benefit.txt"
 foreach ($vmName in $Servers)
 {
     
-$subs = @("s-sis-eu-nonprod-01")
+#$subs = @("s-sis-eu-nonprod-01")
 #$subs = @("s-sis-eu-nonprod-01","s-sis-ap-prod-01","s-sis-eu-prod-01","s-sis-am-prod-01","s-sis-am-nonprod-01")
 #$subs = @("s-sis-eu-nonprod-01")
 foreach ($sub in $subs)
