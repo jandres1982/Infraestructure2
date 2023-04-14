83850de567b43fd96f093baa867afa40b03fbcf1
Antonio <antoniovicente.vento@schindler.com>
2023-04-14 13:59:54 +0200

    AzureMonitoringWindowsAgent

---
+++ scripts/AzureMonitorAgent/AzureMonitorWindowsAgent.ps1
@@ -0,0 +1,5 @@
+#Install
+Set-AzVMExtension -Name AzureMonitorWindowsAgent -ExtensionType AzureMonitorWindowsAgent -Publisher Microsoft.Azure.Monitor -ResourceGroupName "RG-CIS-PROD-WSUSSERVER-01"  -VMName "shhwsr1238" -Location "NorthEurope" -TypeHandlerVersion "1.14" -EnableAutomaticUpgrade $true
+
+#Remove the old one
+Remove-AzVMExtension -Name Microsoft.Insights.LogAnalyticsAgent -ResourceGroupName "RG-CIS-PROD-WSUSSERVER-01"  -VMName "shhwsr1238" -Confirm:$false -force
