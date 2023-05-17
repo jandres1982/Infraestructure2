ac2095286fc1d44ba634deb94746a18ef0aadf86
Antonio <antoniovicente.vento@schindler.com>
2023-05-17 10:11:35 +0200

    ip list

--- scripts/Network/Datalakes/get_ips_st_acc.ps1
+++ scripts/Network/Datalakes/get_ips_st_acc.ps1
@@ -1,9 +1,8 @@
 $myvault = "dlsqualssot04"
 $rg = "rg-gis-qual-ssot-01"
-$ipranges = gc "ipranges.txt"
-foreach ($iprange in $ipranges)
-{
-    Add-AzStorageAccountNetworkRule -ResourceGroupName $rg -Name $myvault  -IPRule (@{IPAddressOrRange=$iprange;Action="allow"})
+$ipranges = get-comnmand "ipranges.txt"
+foreach ($iprange in $ipranges) {
+    Add-AzStorageAccountNetworkRule -ResourceGroupName $rg -Name $myvault  -IPRule (@{IPAddressOrRange = $iprange; Action = "allow" })
 }
 
 $stacc = "dlsqualssot04"
@@ -11,6 +10,9 @@ $rg = "rg-gis-qual-ssot-01"
 $ips = az storage account network-rule list -g $rg --account-name $stacc
 $values = $ips | ConvertFrom-Json
 $IpList = $values.ipRules.ipAddressOrRange
+
+
+
 
 #az account set --subscription s-sis-eu-nonprod-01
 #az cloud list --output table
