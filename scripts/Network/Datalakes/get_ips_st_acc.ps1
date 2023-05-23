0ff7c92c488a8b785b75fe8b668adb06d61edab5
Antonio <antoniovicente.vento@schindler.com>
2023-05-23 09:16:39 +0200

    apply changes Ips allow and CRD

--- scripts/Network/Datalakes/get_ips_st_acc.ps1
+++ scripts/Network/Datalakes/get_ips_st_acc.ps1
@@ -1,13 +1,6 @@
-$myvault = "dlsqualssot04"
-$rg = "rg-gis-qual-ssot-01"
-$ipranges = get-comnmand "ipranges.txt"
-foreach ($iprange in $ipranges) {
-    Add-AzStorageAccountNetworkRule -ResourceGroupName $rg -Name $myvault  -IPRule (@{IPAddressOrRange = $iprange; Action = "allow" })
-}
-
-$stacc = "dlsqualssot04"
-$rg = "rg-gis-qual-ssot-01"
-$ips = az storage account network-rule list -g $rg --account-name $stacc
+$stacc_source = "dlsqualssot04"
+$rg_source = "rg-gis-qual-ssot-01"
+$ips = az storage account network-rule list -g $rg_source --account-name $stacc_source
 $values = $ips | ConvertFrom-Json
 $IpList = $values.ipRules.ipAddressOrRange
 
@@ -17,3 +10,14 @@ $IpList = $values.ipRules.ipAddressOrRange
 #az account set --subscription s-sis-eu-nonprod-01
 #az cloud list --output table
 #az cloud set --name AzureCloud
+
+#$ipranges = gc "ipranges.txt"
+
+$stacc = "dlsdevcs01"
+$rg = "rg-gis-dev-ssot-01"
+$sub = "s-sis-eu-nonprod-01"
+
+Set-AzContext -Subscription $sub
+foreach ($iprange in $IpList) {
+    Add-AzStorageAccountNetworkRule -ResourceGroupName $rg -Name $stacc  -IPRule (@{IPAddressOrRange = $iprange; Action = "allow" })
+}
