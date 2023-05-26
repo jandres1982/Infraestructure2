83f3dbae1384fdbf46b726c704d140085adb021c
Antonio <antoniovicente.vento@schindler.com>
2023-05-26 08:28:03 +0200

    wub

--- scripts/Server_provisioning/Install_packages/WindowsUpdateB.ps1
+++ scripts/Server_provisioning/Install_packages/WindowsUpdateB.ps1
@@ -1,49 +1,29 @@
-   
-  <#verifica si esta instalada la libreria previa a la instalacion#> 
-  $provider= Get-Package -Name Nuget -EA Ignore
-  if ($provider) {
-    $installed = $true
-  }else{
-      $installed = $false
-  }
-  if ($installed -eq $false) {
-      Install-Package -Name Nuget -Force 
-  }
-  
-  
-  
-  <#verifica si esta instalado el modulo de actualizaciones de windows#>
-  $moduleInstalled = Get-Module -Name PSWindowsUpdate -ListAvailable
-  
-  if ($moduleInstalled) {
-      $isModuleInstalled = $true
-  } else {
-      $isModuleInstalled = $false
-  }
-  <#ejecuta el analisis y la instalacion de las actualizaciones del equipo,
-   primero verificando si existe alguna disponible#>
-  if ($isModuleInstalled -eq $true) {
-      $updatesVerify = Get-WindowsUpdate 
-  if($updatesVerify){
-  $verify = $true
-  }else{
-      $verify = $false
-  }
-      if ($verify -eq $true) {
-          
-          Get-WindowsUpdate -AcceptAll -Install -IgnoreReboot
-  Write-OutPut "Updates has been installed"
-  
-              }else{
-                   Write-Output "No Updates are needed"
-                      }
-  }else{
-      Install-Module PSWindowsUpdate -Force
-      if ($verify -eq $true) {
-          Get-WindowsUpdate -AcceptAll -Install -IgnoreReboot
-              }else{
-                   Write-Output "No Updates are needed"
-                      }
-      
-      
-     }
+###
+$provider = Get-Package -Name Nuget -EA Ignore
+if ($provider) {
+Write-Output "Nuget is correctly installed"
+}
+else {
+    Install-Package -Name Nuget -Force 
+}
+
+$moduleInstalled = Get-Module -Name PSWindowsUpdate -ListAvailable
+
+If ($moduleInstalled)
+{
+    Write-Output "Windows Update Module is available"
+}else
+    {
+Install-Module PSWindowsUpdate -Force
+    }
+
+    Import-Module PSWindowsUpdate
+###
+$updatesVerify = Get-WindowsUpdate 
+    if ($updatesVerify) {
+        Get-WindowsUpdate -AcceptAll -Install -IgnoreReboot
+        Write-OutPut "Updates has been installed" 
+    }
+    else {
+        Write-OutPut "No updates needed"
+    }
