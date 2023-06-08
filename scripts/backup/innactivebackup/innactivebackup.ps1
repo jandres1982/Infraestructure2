4670dc6b382e810dc0382322f5aa10453d49bddc
Antonio <antoniovicente.vento@schindler.com>
2023-06-08 16:55:28 +0200

    Backup To Remove VM

--- scripts/backup/innactivebackup/innactivebackup.ps1
+++ scripts/backup/innactivebackup/innactivebackup.ps1
@@ -3,28 +3,26 @@ $VMData = $InnactiveBackup | Where-Object { $_."Datasource Type" -eq "Microsoft.
 $Short = "vm-short-01am-01"
 $Medium = "vm-medium-01am-01"
 $Long = "vm-long-01am-01"
-
+Remove-Item "C:\Users\ventoa1\OneDrive - Schindler\Azure_Devops\Infraestructure\scripts\backup\innactivebackup\backup_to_remove.csv" -Force -ErrorAction SilentlyContinue
+Set-Location -path "C:\Users\ventoa1\OneDrive - Schindler\Azure_Devops\Infraestructure\scripts\backup\innactivebackup\"
 [datetime]$CurrentDate30 = $(Get-date).AddDays(-30)
 
 foreach ($vm in $vmdata) {
     If ($vm.Policy -eq $Short ) {
-        $VmPolicy = $vm.Policy
         Write-Host "---------"
+        $VmPolicy = $vm.Policy
         Write-Host "This server has policy $VmPolicy"
         $vmName = $Vm."Backup Instance"
         Write-Output $vmName
         [datetime]$date = $vm."Latest Recovery Point"
-        
-        if ($date -lt $CurrentDate30)
-        {
-        Write-host "Can be removed"
-        Write-Output "$vmName;$VmPolicy;$date" | out-file -Append .\backup_to_remove.csv
-        }else
-            {
-                Write-Host "---------"
-                Write-Output $Vm."Backup Instance"
-                Write-Output "Cannot be removed"
-            }
+        if ($date -lt $CurrentDate30) {
+            Write-host "Can be removed"
+            Write-Output "$vmName;$VmPolicy;$date" | out-file -Append .\To_remove.csv
+        }
+        else {
+            Write-Output $Vm."Backup Instance"
+            Write-Output "Cannot be removed"
+        }
     }
 }
 
@@ -32,51 +30,48 @@ foreach ($vm in $vmdata) {
 
 foreach ($vm in $vmdata) {
     If ($vm.Policy -eq $Medium ) {
-        $VmPolicy = $vm.Policy
         Write-Host "---------"
+        $VmPolicy = $vm.Policy
+        
         Write-Host "This server has policy $VmPolicy"
         Write-Output $Vm."Backup Instance"
         [datetime]$date = $vm."Latest Recovery Point"
         $vmName = $Vm."Backup Instance"
 
-        
-
-        if ($date -lt $CurrentDate90)
-        {
-        Write-host "Can be removed"
-        Write-Output "$vmName;$VmPolicy;$date" | out-file -Append .\backup_to_remove.csv
-        }else
-            {
-                Write-Host "---------"
-                Write-Output $Vm."Backup Instance"
-                Write-Output "Cannot be removed"
-            }
+        if ($date -lt $CurrentDate90) {
+            Write-host "Can be removed"
+            Write-Output "$vmName;$VmPolicy;$date" | out-file -Append .\To_remove.csv
+        }
+        else {
+            
+            Write-Output $Vm."Backup Instance"
+            Write-Output "Cannot be removed"
+        }
     }
 }
-
 
 [datetime]$CurrentDate360 = $(Get-date).AddDays(-360)
 
 foreach ($vm in $vmdata) {
     If ($vm.Policy -eq $Long) {
+        Write-Host "---------"
         $VmPolicy = $vm.Policy
         $vmName = $Vm."Backup Instance"
-        Write-Host "---------"
+        Write-host $vmName
         Write-Host "This server has policy $VmPolicy"
         [datetime]$date = $vm."Latest Recovery Point"
-        Write-Output ""
         $vmName = $Vm."Backup Instance"
         
-
-        if ($date -lt $CurrentDate360)
-        {
-        Write-host "Can be removed"
-        Write-Output "$vmName;$VmPolicy;$date" | out-file -Append .\backup_to_remove.csv
-        }else
-            {
-                Write-Host "---------"
-                Write-Output $Vm."Backup Instance"
-                Write-Output "Cannot be removed"
-            }
+        if ($date -lt $CurrentDate360) {
+            Write-host "Can be removed"
+            Write-Output "$vmName;$VmPolicy;$date" | out-file -Append .\To_remove.csv
+        }
+        else {
+            #Write-Output $Vm."Backup Instance"
+            Write-Output "Cannot be removed"
+        }
     }
 }
+
+$VMNone = $vmdata | Where-Object {$_.Policy -eq "(none)"}
+$VMnone | export-csv -Delimiter ";" -LiteralPath "No_policy.csv"
