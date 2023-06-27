7b76689331e8a19f19cfca0a8ae4a6895937c97a
Antonio <antoniovicente.vento@schindler.com>
2023-06-27 15:07:41 +0200

    Checking page file in T before changing

--- scripts/Server_provisioning/Install_packages/Set_PF_C.ps1
+++ scripts/Server_provisioning/Install_packages/Set_PF_C.ps1
@@ -1,2 +1,12 @@
-Set-Itemproperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management' -Name 'PagingFiles' -value 'C:\pagefile.sys 0 0'
-restart-computer -force
+if ($(Get-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management' -Name 'PagingFiles').PagingFiles -eq 'T:\pagefile.sys 0 0') 
+{
+    Write-Output "All Ok Page File is already in T:\"
+}
+else {
+    Set-Itemproperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management' -Name 'PagingFiles' -value 'C:\pagefile.sys 0 0'
+    restart-computer -force
+}
+
+
+
+
