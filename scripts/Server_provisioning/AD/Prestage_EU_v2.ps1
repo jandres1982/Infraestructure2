21806c0f81ecc25ebd868704d410b959c8431ef8
Antonio <antoniovicente.vento@schindler.com>
2023-07-06 10:20:31 +0200

    test

--- scripts/Server_provisioning/AD/Prestage_EU_v2.ps1
+++ scripts/Server_provisioning/AD/Prestage_EU_v2.ps1
@@ -1,15 +1,15 @@
-param([string]$vm,[string]$function,[string]$joinuser,$joinpw)
+param([string]$vm,[string]$function)
 $vm = $vm.ToUpper()
 $KG = $vm.Substring(0,3)
 $function = "$KG Windows Server $function"
 
-#$pw = $(Get-AzKeyVaultSecret -VaultName 'kv-prod-devopsagents-01' -Name 'JoinPw').SecretValue
-#$pw = $joinpw.SecretValue
+$pw = $(Get-AzKeyVaultSecret -VaultName 'kv-prod-devopsagents-01' -Name 'JoinPw').SecretValue
+$pw = $joinpw.SecretValue
 
 #$user = $(Get-AzKeyVaultSecret -VaultName 'kv-prod-devopsagents-01' -Name 'JoinUser' -AsPlainText)
 #$user = 
-Write-Output "this is join user: $joinuser"
-Write-Output "this is join pw: $joinpw"
+echo "this is join user: $joinuser"
+echo "this is join pw: $joinpw"
 #$cred = New-Object System.Management.Automation.PSCredential -ArgumentList ($joinuser, $joinpw)
 
 #New-ADComputer -Name $vm -Path "OU=EU,OU=Servers,OU=NBI12,DC=global,DC=schindler,DC=com" -PasswordNotRequired $false -Description $Function -Credential $cred
