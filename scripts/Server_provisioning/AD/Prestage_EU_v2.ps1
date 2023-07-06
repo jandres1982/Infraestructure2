685dfe9faed8dbd2cbc89b85bb095c6a84a3710d
Antonio <antoniovicente.vento@schindler.com>
2023-07-06 10:00:38 +0200

    test

--- scripts/Server_provisioning/AD/Prestage_EU_v2.ps1
+++ scripts/Server_provisioning/AD/Prestage_EU_v2.ps1
@@ -1,4 +1,4 @@
-param([string]$vm,[string]$function,$joinuser,$joinpw)
+param([string]$vm,[string]$function,[string]$joinuser,$joinpw)
 $vm = $vm.ToUpper()
 $KG = $vm.Substring(0,3)
 $function = "$KG Windows Server $function"
@@ -8,11 +8,11 @@ $function = "$KG Windows Server $function"
 
 #$user = $(Get-AzKeyVaultSecret -VaultName 'kv-prod-devopsagents-01' -Name 'JoinUser' -AsPlainText)
 #$user = 
-Write-Output "$joinuser"
-Write-Output "$joinpw"
-$cred = New-Object System.Management.Automation.PSCredential -ArgumentList ($joinuser, $joinpw)
+Write-Output "this is join user: $joinuser"
+Write-Output "this is join pw: $joinpw"
+#$cred = New-Object System.Management.Automation.PSCredential -ArgumentList ($joinuser, $joinpw)
 
-New-ADComputer -Name $vm -Path "OU=EU,OU=Servers,OU=NBI12,DC=global,DC=schindler,DC=com" -PasswordNotRequired $false -Description $Function -Credential $cred
+#New-ADComputer -Name $vm -Path "OU=EU,OU=Servers,OU=NBI12,DC=global,DC=schindler,DC=com" -PasswordNotRequired $false -Description $Function -Credential $cred
 write-host "$vm and $Function"
 
 #$pw = Get-AzKeyVaultSecret -VaultName 'kv-prod-devopsagents-01' -Name 'JoinPwTstglobal'
