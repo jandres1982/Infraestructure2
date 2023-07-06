a63b37f3d5c6bc151b95b60fd0be94112686f90c
Antonio <antoniovicente.vento@schindler.com>
2023-07-06 09:52:35 +0200

    setting variables

--- scripts/Server_provisioning/AD/Prestage_EU_v2.ps1
+++ scripts/Server_provisioning/AD/Prestage_EU_v2.ps1
@@ -1,11 +1,14 @@
-param([string]$vm,[string]$function,$user,$pw)
+param([string]$vm,[string]$function,$joinuser,$joinpw)
 $vm = $vm.ToUpper()
 $KG = $vm.Substring(0,3)
 $function = "$KG Windows Server $function"
 
 #$pw = $(Get-AzKeyVaultSecret -VaultName 'kv-prod-devopsagents-01' -Name 'JoinPw').SecretValue
+#$pw = $joinpw.SecretValue
+
 #$user = $(Get-AzKeyVaultSecret -VaultName 'kv-prod-devopsagents-01' -Name 'JoinUser' -AsPlainText)
-$cred = New-Object System.Management.Automation.PSCredential -ArgumentList ($User, $pw)
+#$user = 
+$cred = New-Object System.Management.Automation.PSCredential -ArgumentList ($joinuser, $joinpw)
 
 New-ADComputer -Name $vm -Path "OU=EU,OU=Servers,OU=NBI12,DC=global,DC=schindler,DC=com" -PasswordNotRequired $false -Description $Function -Credential $cred
 write-host "$vm and $Function"
