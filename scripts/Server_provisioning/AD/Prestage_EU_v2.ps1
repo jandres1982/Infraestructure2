f410a46817e52662ef881509e12a60b28c65fae2
Antonio <antoniovicente.vento@schindler.com>
2023-07-06 12:55:32 +0200

    secure string

--- scripts/Server_provisioning/AD/Prestage_EU_v2.ps1
+++ scripts/Server_provisioning/AD/Prestage_EU_v2.ps1
@@ -9,8 +9,10 @@ $function = "$KG Windows Server $function"
 #$user = $(Get-AzKeyVaultSecret -VaultName 'kv-prod-devopsagents-01' -Name 'JoinUser' -AsPlainText)
 #$user = 
 echo "this is join user: $joinuser"
-echo "this is join pw: $joinpw.SecretValue"
-$cred = New-Object System.Management.Automation.PSCredential -ArgumentList ($joinuser, $joinpw)
+echo "this is join pw: $joinpw"
+$joinpw = $joinpw | ConvertTo-SecureString
+echo "this is join pw converted to secure string: $joinpw"
+$cred = New-Object System.Management.Automation.PSCredential -ArgumentList ($joinuser,$joinpw)
 
 New-ADComputer -Name $vm -Path "OU=EU,OU=Servers,OU=NBI12,DC=global,DC=schindler,DC=com" -PasswordNotRequired $false -Description $Function -Credential $cred
 write-host "$vm and $Function"
