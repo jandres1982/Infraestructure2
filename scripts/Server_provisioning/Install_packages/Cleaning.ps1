cf9d400f066143b026c04a1b4518cfbb3f8543f7
Antonio <antoniovicente.vento@schindler.com>
2023-07-07 12:22:28 +0200

    Prestage DMZ

--- scripts/Server_provisioning/Install_packages/Cleaning.ps1
+++ scripts/Server_provisioning/Install_packages/Cleaning.ps1
@@ -1,3 +1,3 @@
-Unregister-ScheduledTask -TaskName "Join Domain" -TaskPath "\" -Confirm:$false
+#Unregister-ScheduledTask -TaskName "Join Domain" -TaskPath "\" -Confirm:$false
 Unregister-ScheduledTask -TaskName "Local Admin Group" -TaskPath "\" -Confirm:$false
 Remove-Item -Path 'C:\provision' -Recurse -Force
