78dd3682c8e41cedc60cf03416f953a622679ad0
Antonio <antoniovicente.vento@schindler.com>
2023-07-10 12:12:40 +0200

    s was missing

--- scripts/Server_provisioning/AD/Prestage_Tstglobal.ps1
+++ scripts/Server_provisioning/AD/Prestage_Tstglobal.ps1
@@ -6,7 +6,7 @@ $vm = $vm.ToUpper()
 $KG = $vm.Substring(0,3)
 $function = "$KG Windows Server $function"
 #Invoke-Command -ComputerName "shhwsr2306.dmz2.schindler.com" -Credential $cred -ScriptBlock {param($vm,$function,$cred) New-ADComputer -Name $vm -Path "OU=000,OU=Servers,OU=NBI12,DC=dmz2,DC=schindler,DC=com" -PasswordNotRequired $false -Description $function -credential $cred} -ArgumentList $vm,$function,$cred
-$ScriptingServer = "tstshhwsr0326.tstglobal.chindler.com"
+$ScriptingServer = "tstshhwsr0326.tstglobal.schindler.com"
 $parameters = @{
     ComputerName = $ScriptingServer
     Credential = $cred
