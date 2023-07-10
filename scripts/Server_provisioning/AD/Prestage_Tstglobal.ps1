79a36202ed38e1dc5715e9eaf0025e8b1e287925
Antonio <antoniovicente.vento@schindler.com>
2023-07-10 12:16:23 +0200

    changing OU

--- scripts/Server_provisioning/AD/Prestage_Tstglobal.ps1
+++ scripts/Server_provisioning/AD/Prestage_Tstglobal.ps1
@@ -10,7 +10,7 @@ $ScriptingServer = "tstshhwsr0326.tstglobal.schindler.com"
 $parameters = @{
     ComputerName = $ScriptingServer
     Credential = $cred
-    ScriptBlock = {param($vm,$function,$cred) New-ADComputer -Name $vm -Path "OU=000,OU=Servers,OU=NBI12,DC=dmz2,DC=schindler,DC=com" -PasswordNotRequired $false -Description $function -credential $cred}
+    ScriptBlock = {param($vm,$function,$cred) New-ADComputer -Name $vm -Path "OU=EU,OU=Servers,OU=NBI12,DC=tstglobal,DC=schindler,DC=com" -PasswordNotRequired $false -Description $function -credential $cred}
     ArgumentList =  $vm,$function,$cred
 }
 Invoke-Command @parameters
