1dd10609c97c56b0d5ec7636e60c96224d57ef68
Nahum Sancho Puri <nahum.sancho@schindler.com>
2023-07-13 12:14:12 +0000

    Adding data collector s-sis-am-prod-01

--- scripts/Sentinel_Data_Collector_Rule_Associate/sentinel_dcr_associate.ps1
+++ scripts/Sentinel_Data_Collector_Rule_Associate/sentinel_dcr_associate.ps1
@@ -1,6 +1,6 @@
 # Source info webpage https://learn.microsoft.com/en-us/powershell/module/az.monitor/new-azdatacollectionruleassociation?view=azps-10.1.0
 $dcr = "/subscriptions/505ead1a-5a5f-4363-9b72-83eb2234a43d/resourceGroups/rg-gis-prod-sentinel-01/providers/Microsoft.Insights/dataCollectionRules/Windows_Security_Events"
-$subs = @("s-sis-eu-nonprod-01","s-sis-am-nonprod-01","s-sis-ch-nonprod-01","s-sis-ch-prod-01","s-sis-ap-prod-01")
+$subs = @("s-sis-eu-nonprod-01","s-sis-am-nonprod-01","s-sis-ch-nonprod-01","s-sis-ch-prod-01","s-sis-ap-prod-01","s-sis-am-prod-01")
 
 foreach ($sub in $subs)
 {
