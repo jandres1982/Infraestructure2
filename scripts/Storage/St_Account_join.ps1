7aff6a274b6f53cf88110dbc9010abf221bb2602
Alberto Delgado Alvarez <alberto.delgado@schindler.com>
2023-07-13 07:09:54 +0000

    Updated St_Account_join.ps1 sub selection + variables

--- scripts/Storage/St_Account_join.ps1
+++ scripts/Storage/St_Account_join.ps1
@@ -1,7 +1,6 @@
 Import-Module -Name AzFilesHybrid
-$Storage_account = "stqualsqlfileshare02"
+set-azcontext -subscription $(sub)
 $Domain = "global.schindler.com"
 $OU = "OU=zzz,OU=servers,OU=NBI12,DC=global,DC=schindler,DC=com"
-$ResourceGroupName = "rg-shh-qual-rmp-01"
 #You will need to install powershell module AzFilesHybrid
-Join-AzStorageAccountForAuth -ResourceGroupName $ResourceGroupName -StorageAccountName $Storage_account -DomainAccountType ComputerAccount -OrganizationalUnitDistinguishedName $OU
+Join-AzStorageAccountForAuth -ResourceGroupName $(rg) -StorageAccountName $(stname) -DomainAccountType ComputerAccount -OrganizationalUnitDistinguishedName $OU
