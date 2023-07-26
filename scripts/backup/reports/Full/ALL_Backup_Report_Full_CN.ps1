089a54c59eb484b101914fe20e3909d2ff62ca24
Antonio <antoniovicente.vento@schindler.com>
2023-07-26 11:18:19 +0200

    china backup report

--- scripts/backup/reports/Full/ALL_Backup_Report_Full_CN.ps1
+++ scripts/backup/reports/Full/ALL_Backup_Report_Full_CN.ps1
@@ -2,7 +2,7 @@
 #$subs = @("s-sis-eu-nonprod-01","s-sis-eu-prod-01","s-sis-am-prod-01","s-sis-am-nonprod-01","s-sis-ap-prod-01","s-sis-ch-prod-01","s-sis-ch-nonprod-01")
 $subs = $(Get-AzSubscription).name
 $date = $(get-date -format yyyy-MM-ddTHH-mm)
-$kg = "ALL"
+$kg = "CN"
 ###################################################################
 
 $vmBackupReport = [System.Collections.ArrayList]::new()
@@ -75,8 +75,7 @@ $vmBackupReport | Export-Csv $report -NoTypeInformation | Select-Object -Skip 1 
 
 $PSEmailServer = "smtp.eu.schindler.com"
 $From = "scc-support-zar.es@schindler.com"
-#$to = "gda_usr_dcff050b-8326-48c9-8bf9-61f8de7e89f0@schindler.com","gdl_usr_7aabcc1e-97e6-4283-9271-c04245556940@cloud.schindler.com"
-$to = "antoniovicente.vento@schindler.com"
+$to = "gda_usr_dcff050b-8326-48c9-8bf9-61f8de7e89f0@schindler.com","gdl_usr_7aabcc1e-97e6-4283-9271-c04245556940@cloud.schindler.com","paul.henke@schindler.com"
 $Subject = "Backup Report $kg Servers"
 #$Filename = Get-ChildItem $Path -Name "Att*" | select -Last 1
 $Attachment = $report
