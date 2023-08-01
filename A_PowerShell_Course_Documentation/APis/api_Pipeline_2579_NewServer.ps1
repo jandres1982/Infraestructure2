7b18ec5c2be1df7f8cb251c8051d44dc77ca9be1
Antonio <antoniovicente.vento@schindler.com>
2023-08-01 12:48:20 +0200

    NewServer api pipeline

--- A_PowerShell_Course_Documentation/APis/api_Pipeline_2579_NewServer.ps1
+++ A_PowerShell_Course_Documentation/APis/api_Pipeline_2579_NewServer.ps1
@@ -4,12 +4,28 @@ $projectName = "SIS-Azure_OP-Automation"
 $personalAccessToken = "2iorsurunvakcaqqjdevtggtm23gabou4llrbkm5y4awwvm35ljq"
 $pipelineId = "1764"
 $varname1 = "vm"
-$varvalue1 = "zzzwsr0012"
+$varvalue1 = ""
 $varname2 = "domain"
-$varvalue2 = "global"
+$varvalue2 = ""
+$varname3 = "sub"
+$varvalue3 = ""
+$varname4 = "vmSize"
+$varvalue4 = ""
+$varname5 = "datasize"
+$varvalue5 = ""
+$varname6 = "existingSubnetName"
+$varvalue6 = ""
+$varname7 = "zone"
+$varvalue7 = ""
+$varname8 = "osversion"
+$varvalue8 = ""
+$varname8 = "ip"
+$varvalue8 = ""
 
 # Define the API endpoint
 $apiUrl = "$organizationUrl/$projectName/_apis/pipelines/$pipelineId/runs?api-version=6.0-preview.1"
+
+sub=$(sub) vmSize=$(vmsize) datasize=$(drive_d_size_GB) vmname=$(vm) existingSubnetName=$(subnetName) zone=$(zone) osversion=$(OSversion) ip=$(ip)
 
 # Create a JSON payload for the request with variables
 $variables = @{
@@ -19,6 +35,27 @@ $variables = @{
         }
                 ($varname2) = @{
             value = $varvalue2
+        }
+        ($varname3) = @{
+            value = $varvalue3
+        }
+                ($varname4) = @{
+            value = $varvalue4
+        }
+        ($varname5) = @{
+            value = $varvalue5
+        }
+                ($varname6) = @{
+            value = $varvalue6
+        }
+        ($varname6) = @{
+            value = $varvalue6
+        }
+                ($varname7) = @{
+            value = $varvalue7
+        }
+        ($varname8) = @{
+            value = $varvalue8
         }
     }
 }
